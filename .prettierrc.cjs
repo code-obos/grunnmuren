07f13307a47794be15387cd26adaa61187c2bb10
Alexander Bjerkan <alexander.bjerkan@obos.no>
2022-05-10 07:33:44 +0200

    add Tailwind Prettier plugin

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,4 +1,6 @@
 module.exports = {
   singleQuote: true,
   trailingComma: 'all',
+  // Workaround symlinked plugins (pnpm) not being autodetected https://github.com/prettier/prettier/issues/8056
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
 };
