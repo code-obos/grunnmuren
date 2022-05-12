888d291e7faee870b1258d849af792e49ec00423
Alexander Bjerkan <alexander.bjerkan@obos.no>
2022-05-12 18:49:49 +0200

    fix banner container width

--- packages/react/src/Banner/Banner.tsx
+++ packages/react/src/Banner/Banner.tsx
@@ -26,9 +26,9 @@ export const Banner = (props: BannerProps) => {
       className={classNames(className, bgColor, 'py-8 px-4 md:py-14')}
       {...rest}
     >
-      <div className="<md:flex-wrap flex justify-center gap-4 md:flex-row md:gap-12">
+      <div className="<md:flex-wrap container flex gap-4 md:flex-row md:gap-12">
         {image}
-        <div className="w-prose flex-initial">
+        <div className="w-prose">
           {heading && <h2 className="mb-4">{heading}</h2>}
           {children}
         </div>
