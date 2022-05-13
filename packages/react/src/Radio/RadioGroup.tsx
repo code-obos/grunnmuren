cc28fd90c26537514d8eff8ede40d3ef750ac31d
Alexander Bjerkan <alexander.bjerkan@obos.no>
2022-05-13 08:12:23 +0200

    radio buttons

---
+++ packages/react/src/Radio/RadioGroup.tsx
@@ -0,0 +1,84 @@
+import { useMemo, useId, useCallback } from 'react';
+import classNames from 'clsx';
+import { RadioContext } from './RadioContext';
+import { FormHelperText, FormLabel } from '../';
+
+export interface RadioGroupProps
+  extends Omit<React.ComponentPropsWithoutRef<'div'>, 'onChange'> {
+  /** The value of the radio button to be initially selected. For uncontrolled usage */
+  defaultValue?: string;
+  /** Help text for the radio group. */
+  description?: string;
+  /** Error text for the radio group. */
+  error?: string;
+  /** The `name` attribute for the radio buttons. */
+  name: string;
+  /** The label for the radio group */
+  label?: string;
+  onChange?(value: string): void;
+  /** Whether the  */
+  required?: boolean;
+  /** The value of the selected radio button. For controlled usage */
+  value?: string;
+}
+
+export const RadioGroup = (props: RadioGroupProps) => {
+  const isControlled = 'value' in props;
+
+  const {
+    className,
+    defaultValue,
+    description,
+    children,
+    label,
+    name,
+    onChange: onChangeProp,
+    required,
+    value,
+    ...rest
+  } = props;
+
+  const onChange = useCallback(
+    (event) => {
+      const nextValue = event.target.value;
+      onChangeProp?.(nextValue);
+    },
+    [onChangeProp],
+  );
+
+  const group = useMemo(
+    () => ({
+      defaultValue,
+      isControlled,
+      name,
+      onChange,
+      required,
+      value,
+    }),
+    [defaultValue, isControlled, name, onChange, required, value],
+  );
+
+  const groupId = useId();
+  const labelId = `${groupId}:label`;
+  const helpId = `${groupId}:help`;
+
+  return (
+    <RadioContext.Provider value={group}>
+      <div
+        aria-describedby={description ? helpId : undefined}
+        aria-labelledby={label ? labelId : undefined}
+        className={classNames(className, 'flex flex-col gap-4')}
+        role="radiogroup"
+        {...rest}
+      >
+        {label && (
+          <FormLabel id={labelId} isRequired={required}>
+            {label}
+          </FormLabel>
+        )}
+        {children}
+        <FormHelperText id={helpId}>{description}</FormHelperText>
+      </div>
+    </RadioContext.Provider>
+  );
+};
