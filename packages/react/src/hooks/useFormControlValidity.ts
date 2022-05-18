ceb4e4f134b42237f52b7e1feb370d79a3b5583e
Alexander Bjerkan <alexander.bjerkan@obos.no>
2022-05-18 12:14:43 +0200

    wrap with useCallback

--- packages/react/src/hooks/useFormControlValidity.ts
+++ packages/react/src/hooks/useFormControlValidity.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, RefObject } from 'react';
+import { useState, useEffect, RefObject, useCallback } from 'react';
 
 type Validity = 'indeterminate' | 'invalid' | 'valid';
 
@@ -15,36 +15,45 @@ export function useFormControlValidity(
   const [validity, setValidity] = useState<Validity>('indeterminate');
   const [validationMessage, setValidationMessage] = useState<string>();
 
-  const onBlur = (event: FocusEvent) => {
-    // this triggers an invalid event, so if it's invalid it's handled by the `onInvalid` handler
-    const isValid = (event.target as HTMLInputElement).checkValidity();
-
-    if (isValid && enabled) {
-      setValidity('valid');
-      setValidationMessage(undefined);
-    }
-  };
-
-  const onInput = (event: Event) => {
-    if (enabled && validity !== 'indeterminate') {
+  const onBlur = useCallback(
+    (event: FocusEvent) => {
       // this triggers an invalid event, so if it's invalid it's handled by the `onInvalid` handler
       const isValid = (event.target as HTMLInputElement).checkValidity();
 
-      if (isValid) {
+      if (isValid && enabled) {
         setValidity('valid');
         setValidationMessage(undefined);
       }
-    }
-  };
+    },
+    [enabled],
+  );
 
-  const onInvalid = (event: Event) => {
-    if (enabled) {
-      event.preventDefault();
-      const message = (event.target as HTMLInputElement).validationMessage;
-      setValidationMessage(message);
-      setValidity('invalid');
-    }
-  };
+  const onInput = useCallback(
+    (event: Event) => {
+      if (enabled && validity !== 'indeterminate') {
+        // this triggers an invalid event, so if it's invalid it's handled by the `onInvalid` handler
+        const isValid = (event.target as HTMLInputElement).checkValidity();
+
+        if (isValid) {
+          setValidity('valid');
+          setValidationMessage(undefined);
+        }
+      }
+    },
+    [enabled, validity],
+  );
+
+  const onInvalid = useCallback(
+    (event: Event) => {
+      if (enabled) {
+        event.preventDefault();
+        const message = (event.target as HTMLInputElement).validationMessage;
+        setValidationMessage(message);
+        setValidity('invalid');
+      }
+    },
+    [enabled],
+  );
 
   useEffect(() => {
     const { current } = ref;
