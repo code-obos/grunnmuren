c81fc1f467e99a95b61af318226eecba3cd37180
Alexander Bjerkan <alexander.bjerkan@obos.no>
2022-06-24 07:10:31 +0200

    fix font path for GH pages

--- packages/react/tailwind.config.cjs
+++ packages/react/tailwind.config.cjs
@@ -1,4 +1,11 @@
+const opts = {};
+
+// Fixes font loading for GH pages where the site path is {org}.github.io/{repo}
+if (process.env.GITHUB_ACTION) {
+  opts.fontBasePath = '/grunnmuren/fonts';
+}
+
 module.exports = {
-  presets: [require('@obosbbl/grunnmuren-tailwind')],
+  presets: [require('@obosbbl/grunnmuren-tailwind')(opts)],
   content: ['./src/**/*.{ts,tsx}'],
 };
