3060fbbeb55bf244ccd7bffa28e85b26c6bcd8cd
Aulon Mujaj <aulon.mujaj@gmail.com>
2022-08-16 09:57:17 +0200

    Added default color, possibilities to add own colors and icon-colors are based on badge-colors

--- packages/react/src/Badge/Badge.tsx
+++ packages/react/src/Badge/Badge.tsx
@@ -9,23 +9,25 @@ export type BadgeColor =
   | 'yellow';
 
 export interface BadgeProps {
-  children: React.ReactNode;
-  color: BadgeColor;
   icon: React.ReactNode;
+
+  /* @default green */
+  color?: BadgeColor;
   className?: string;
+  children: React.ReactNode;
 }
 
 const badgeVariations = {
-  'red-light': 'bg-red-light border-red-light',
-  'green-light': 'bg-green-light border-green-light',
-  green: 'bg-green border-green',
-  'blue-light': 'bg-blue-light border-blue-light',
-  yellow: 'bg-yellow border-yellow',
-  'orange-light': 'bg-orange-light border-orange-light',
+  'red-light': 'bg-red-light border-red-light text-black',
+  'green-light': 'bg-green-light border-green-light text-green',
+  green: 'bg-green border-green text-white',
+  'blue-light': 'bg-blue-light border-blue-light text-blue-dark',
+  yellow: 'bg-yellow border-yellow text-black',
+  'orange-light': 'bg-orange-light border-orange-light text-black',
 } as const;
 
 export const Badge = (props: BadgeProps) => {
-  const { className, color, icon, children } = props;
+  const { className, color = 'green', icon, children } = props;
 
   const badgeVariation = badgeVariations[color];
 
@@ -40,12 +42,11 @@ export const Badge = (props: BadgeProps) => {
       <div
         className={classNames(
           'flex items-center justify-center rounded-l-3xl px-3 py-2',
-          badgeVariation,
         )}
       >
         {icon}
       </div>
-      <div className="bg-white py-2 px-3">{children}</div>
+      <div className="bg-white py-2 px-3 text-black">{children}</div>
     </div>
   );
 };
