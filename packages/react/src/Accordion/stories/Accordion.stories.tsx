9feddca37b350379f633fdd84c51e72b14bc9b22
Aulon Mujaj <aulon.mujaj@gmail.com>
2022-09-06 16:40:58 +0200

    Accordion: changes to stories

--- packages/react/src/Accordion/stories/Accordion.stories.tsx
+++ packages/react/src/Accordion/stories/Accordion.stories.tsx
@@ -1,4 +1,5 @@
 import { Accordion } from '..';
+import { Button } from '../../Button';
 
 export default {
   title: 'Accordion',
@@ -11,8 +12,13 @@ export const Default = () => {
         <Accordion heading="Tittel">This is a text</Accordion>
       </div>
       <div className="my-8 mx-4 flex w-1/3 flex-col gap-1">
-        <Accordion heading="Tittel">This is a text</Accordion>
-        <Accordion heading="Tittel">This is a text</Accordion>
+        <Accordion heading="Tittel">
+          <div className="flex flex-col py-6">
+            This is a text but with not a full width accordion. And it has a
+            button
+            <Button>Button!</Button>
+          </div>
+        </Accordion>
       </div>
     </>
   );
