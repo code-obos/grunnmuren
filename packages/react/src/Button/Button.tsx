88600cf9949282002fb9558754c611804ca5a492
Alexander Bjerkan <alexander.bjerkan@obos.no>
2022-09-22 11:44:21 +0200

    use spinner icon from icon lib

--- packages/react/src/Button/Button.tsx
+++ packages/react/src/Button/Button.tsx
@@ -6,6 +6,7 @@ import React, {
   useContext,
   cloneElement,
 } from 'react';
+import { LoadingSpinner } from '@obosbbl/grunnmuren-icons';
 import { cx } from '@/utils';
 import { ButtonColorContext } from '.';
 
@@ -113,30 +114,11 @@ const Loader = (props: { children: React.ReactNode }) => {
         className="!visible absolute top-1 bottom-1 left-1 right-1 grid place-items-center overflow-hidden bg-inherit"
         aria-hidden
       >
-        <LoadingIcon className="animate-spin" />
+        <LoadingSpinner className="animate-spin" />
       </span>
     </>
   );
 };
-
-// TODO: Load from icon lib when the icon is exported properly from Figma
-const LoadingIcon = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    width="1.25em"
-    height="1.25em"
-    fill="none"
-    viewBox="0 0 24 24"
-    {...props}
-  >
-    <path
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeWidth={4}
-      d="M22 12C22 10.2897 21.5613 8.60798 20.726 7.11558C19.8906 5.62318 18.6864 4.36998 17.2285 3.47575C15.7706 2.58152 14.1077 2.07615 12.3988 2.00795C10.6898 1.93975 8.99195 2.311 7.46743 3.0862C5.9429 3.86141 4.64268 5.01466 3.69102 6.43575C2.73937 7.85683 2.1681 9.49824 2.0318 11.2031C1.89551 12.908 2.19875 14.6193 2.91255 16.1735C3.62634 17.7277 4.72684 19.0729 6.10884 20.0805"
-    />
-    <circle cx={21} cy={18} r={2} fill="currentColor" />
-  </svg>
-);
 
 /**
  * Recurse children to wrap strings with spans so they're targeted by the visibility hidden rule
