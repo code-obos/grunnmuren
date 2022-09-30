1684001b57c49ac027b3f5d13db1ad78d3b2bcfc
Alexander Bjerkan <alexander.bjerkan@obos.no>
2022-09-30 09:54:29 +0200

    fix footer rendering artifact issue

--- packages/react/src/Footer/Footer.tsx
+++ packages/react/src/Footer/Footer.tsx
@@ -14,7 +14,8 @@ export const Footer = (props: FooterProps) => {
       <footer
         className={cx(
           className,
-          'bg-blue pt-18 relative pb-12 text-white before:absolute before:top-0 before:left-0 before:right-0 before:h-6 before:rounded-b-3xl before:bg-white',
+          // calculated height for psuedo element fixes rendering artifict that sometimes occurs on specific screen sizes and zoom levels
+          'bg-blue pt-18 relative pb-12 text-white before:absolute before:-top-px before:left-0 before:right-0 before:h-[calc(1.5rem_+_1px)] before:rounded-b-3xl before:bg-white ',
         )}
         {...rest}
       >
