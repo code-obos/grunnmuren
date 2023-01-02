a926facac774517cb0f6acd1b8ac13ac6df7cee2
Alexander Bjerkan <alexander.bjerkan@obos.no>
2023-01-02 11:06:33 +0100

    remove added focus styles. the radio button already has a circle

--- packages/react/src/Radio/Radio.tsx
+++ packages/react/src/Radio/Radio.tsx
@@ -15,10 +15,7 @@ export const Radio = forwardRef<HTMLInputElement, RadioProps>((props, ref) => {
   return (
     <label className={cx(className, 'flex cursor-pointer gap-2.5')}>
       <input
-        className={cx(
-          'radio focus:border-blue-dark',
-          error && 'focus:border-red border-red',
-        )}
+        className={cx('radio', error && 'border-red')}
         defaultChecked={!isControlled ? rest.value === defaultValue : undefined}
         checked={isControlled ? rest.value === value : undefined}
         name={name}
