30302c32da17b957071cd4cef621c58e3468377c
Aulon Mujaj <aulon.mujaj@gmail.com>
2023-01-16 10:33:08 +0100

    add forwardRef to campaign and card components

--- packages/react/src/Card/CardContent.tsx
+++ packages/react/src/Card/CardContent.tsx
@@ -1,8 +1,13 @@
 import { cx } from '@/utils';
+import { forwardRef } from 'react';
 
 interface CardContentProps extends React.ComponentPropsWithoutRef<'div'> {}
 
-export const CardContent = (props: CardContentProps) => {
-  const { className, ...rest } = props;
-  return <div className={cx(className, 'p-8 md:px-10')} {...rest} />;
-};
+export const CardContent = forwardRef<HTMLDivElement, CardContentProps>(
+  (props, ref) => {
+    const { className, ...rest } = props;
+    return (
+      <div className={cx(className, 'p-8 md:px-10')} {...rest} ref={ref} />
+    );
+  },
+);
