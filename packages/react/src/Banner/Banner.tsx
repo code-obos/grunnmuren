74538d0a9df0d9f4260a3f48188731b74e648f7b
Ragnhild Neset <ragcecnes@gmail.com>
2023-02-23 13:09:07 +0100

    Format.

--- packages/react/src/Banner/Banner.tsx
+++ packages/react/src/Banner/Banner.tsx
@@ -23,7 +23,7 @@ export const Banner = (props: BannerProps) => {
 
   return (
     <article className={cx(className, bgColor, 'py-8 px-4 md:py-14')} {...rest}>
-      <div className="container w-fit flex gap-4 max-md:flex-wrap md:gap-12">
+      <div className="container flex w-fit gap-4 max-md:flex-wrap md:gap-12">
         {image}
         <div className="w-prose">
           {heading && <h2 className="mb-4">{heading}</h2>}
