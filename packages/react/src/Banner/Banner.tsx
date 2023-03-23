0941e09a054af41e6af0eef22dcecb73e828b0c3
Ragnhild Neset <ragcecnes@gmail.com>
2023-03-23 14:09:08 +0100

    Fix banner text being cut off on smaller screens.

--- packages/react/src/Banner/Banner.tsx
+++ packages/react/src/Banner/Banner.tsx
@@ -25,7 +25,7 @@ export const Banner = (props: BannerProps) => {
     <article className={cx(className, bgColor, 'py-8 px-4 md:py-14')} {...rest}>
       <div className="container flex flex-col justify-center gap-4 md:flex-row md:gap-12">
         {image}
-        <div className="w-prose">
+        <div className="max-w-prose">
           {heading && <h2 className="h3 mb-4">{heading}</h2>}
           {children}
         </div>
