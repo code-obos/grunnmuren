53023558ef5c9c0bd3a8ca93f452c5adabf06b7c
aulonm <aulon.mujaj@gmail.com>
2023-05-13 06:21:50 +0000

    Deploying to docs from @ code-obos/grunnmuren@59a94312775c8019e8f143526a241ef4b8c2b663 🚀

---
+++ assets/Banner.stories-cf9e5f84.js
@@ -0,0 +1,14 @@
+'use client';
+import{j as r}from"./jsx-runtime-bb3e4369.js";import{B as n,a as m}from"./TextField-2390f227.js";import"./Alert-19a59fec.js";import"./ButtonColorContext-46d6b033.js";import"./index-f782f570.js";import"./index-8aa52469.js";import"./clsx.m-388a5213.js";import"./icons-00ea3a54.js";const f={title:"Banner"},d={alt:"IIllustrasjon av en mobiltelefon",src:"https://res.cloudinary.com/obosit-prd-ch-clry/image/upload/w_172/f_auto,q_auto/v1573744438/Illustrasjoner%20(Karlstr%C3%B6m)/mobil",height:537,width:520},l=["gray","green","yellow","blue","mint","sky"],e=()=>r.jsx("div",{className:"flex flex-col gap-8",children:l.map(o=>r.jsx(n,{bgColor:o,heading:"Kontakt oss",image:r.jsx(m,{...d}),children:"Dersom du lurer på noe er det bare å ta kontakt med oss. Dersom du lurer på noe er det bare å ta kontakt med oss. Dersom du lurer på noe er det bare å ta kontakt med oss. Dersom du lurer på noe er det bare å ta kontakt med oss. Dersom du lurer på noe er det bare å ta kontakt med oss. Dersom du lurer på noe er det bare å ta kontakt med oss. Dersom du lurer på noe er det bare å ta kontakt med oss."},o))});var t,a,s;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`() => {
+  return <div className="flex flex-col gap-8">
+      {bgcColors.map(bgColor => <Banner bgColor={bgColor} heading="Kontakt oss" image={<BannerImage {...img} />} key={bgColor}>
+          Dersom du lurer på noe er det bare å ta kontakt med oss. Dersom du
+          lurer på noe er det bare å ta kontakt med oss. Dersom du lurer på noe
+          er det bare å ta kontakt med oss. Dersom du lurer på noe er det bare å
+          ta kontakt med oss. Dersom du lurer på noe er det bare å ta kontakt
+          med oss. Dersom du lurer på noe er det bare å ta kontakt med oss.
+          Dersom du lurer på noe er det bare å ta kontakt med oss.
+        </Banner>)}
+    </div>;
+}`,...(s=(a=e.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const x=["Default"];export{e as Default,x as __namedExportsOrder,f as default};
+//# sourceMappingURL=Banner.stories-cf9e5f84.js.map
