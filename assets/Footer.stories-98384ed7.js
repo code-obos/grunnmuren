a94de8e9e723a03bcfa9e4eebdd1c1e6937d53b4
aulonm <aulon.mujaj@gmail.com>
2023-05-27 12:25:41 +0000

    Deploying to docs from @ code-obos/grunnmuren@fcdf91cd41e07051df639058185aa1cecc2a462f 🚀

--- assets/Footer.stories-98384ed7.js
+++
@@ -1,9 +0,0 @@
-'use client';
-import{j as e}from"./jsx-runtime-bb3e4369.js";import{F as a}from"./TextField-12ea4a97.js";import"./index-8aa52469.js";import"./clsx.m-388a5213.js";import"./icons-4d582b7e.js";import"./Alert-65397e23.js";import"./ButtonColorContext-1d69175c.js";import"./index-f782f570.js";const h={title:"Footer"},r=()=>e.jsx(a,{children:e.jsxs("div",{children:["I am mostly empty footer. ",e.jsx("a",{href:"#",children:"This link leads no where."})]})});var o,t,s;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`() => {
-  return <Footer>
-      <div>
-        I am mostly empty footer. <a href="#">This link leads no where.</a>
-      </div>
-    </Footer>;
-}`,...(s=(t=r.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,h as default};
-//# sourceMappingURL=Footer.stories-98384ed7.js.map
