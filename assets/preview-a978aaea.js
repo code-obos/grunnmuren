c7caa0aa3fda2cb746475051f1b2381f1fe2af62
aulonm <aulon.mujaj@gmail.com>
2023-06-10 18:20:56 +0000

    Deploying to docs from @ code-obos/grunnmuren@6696fbf774bbe3c02a47ea905467f661accf7fb8 🚀

---
+++ assets/preview-a978aaea.js
@@ -0,0 +1,3 @@
+'use client';
+import{_ as t}from"./iframe-8fad64ff.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-EYKKDMVH-33af9587.js").then(e=>e.D),["./DocsRenderer-EYKKDMVH-33af9587.js","./iframe-8fad64ff.js","./index-8aa52469.js","./_getPrototype-51a4b175.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-a978aaea.js.map
--- assets/preview-ec5a6710.js
+++
@@ -1,3 +0,0 @@
-'use client';
-import{_ as t}from"./iframe-c722a31d.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-EYKKDMVH-1558461f.js").then(e=>e.D),["./DocsRenderer-EYKKDMVH-1558461f.js","./iframe-c722a31d.js","./index-8aa52469.js","./_getPrototype-51a4b175.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-ec5a6710.js.map
