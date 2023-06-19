5a157489ce0260459e7e4cb0b5fdac6ff99ab506
aulonm <aulon.mujaj@gmail.com>
2023-06-19 10:00:17 +0000

    Deploying to docs from @ code-obos/grunnmuren@ea10f873557a0f8500868c8d9f0ddfe7691df9a3 🚀

--- assets/preview-1be0b75b.js
+++
@@ -1,3 +0,0 @@
-'use client';
-import{_ as t}from"./iframe-f3193237.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-EYKKDMVH-3cb04ea9.js").then(e=>e.D),["./DocsRenderer-EYKKDMVH-3cb04ea9.js","./iframe-f3193237.js","./index-8aa52469.js","./_getPrototype-51a4b175.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-1be0b75b.js.map
