1e3032ea0e3dfc1cb10f3500750af7da8b6a0a1c
Aulon Mujaj <aulon.mujaj@gmail.com>
2023-08-23 15:42:33 +0200

    Add icons-react

--- packages/icons-react/scripts/utils.mjs
+++ packages/icons-react/scripts/utils.mjs
@@ -2,7 +2,7 @@ import fs from 'fs-extra';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-export const __dirname = path.dirname(fileURLToPath(require.resolve(import.meta.url)));
+export const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // Assumes the folder only contains SVGs. Poor man's glob
 export function listSvgs(dirPath) {
