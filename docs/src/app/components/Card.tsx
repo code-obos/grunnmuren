84002e4bed452174a65fca26f4bb1a5ba32a72f6
Håkon Drangsland <46884914+haakondrang@users.noreply.github.com>
2023-08-24 09:11:17 +0200

    Finalize frontend

--- docs/src/app/components/Card.tsx
+++ docs/src/app/components/Card.tsx
@@ -1,23 +1,26 @@
 import Link from 'next/link';
 import { FC } from 'react';
+import { ArrowRight } from '@obosbbl/grunnmuren-icons-react';
 
 interface Props {
   title: string;
   content: string;
+  href: string;
 }
 
-const Card: FC<Props> = ({ title, content }) => {
+const Card: FC<Props> = ({ title, content, href }) => {
   return (
-    <div>
-      <div className="shadow-gray-concrete hover:border-gray-concrete relative my-8 flex max-w-prose flex-col items-center rounded-xl border-2 border-white px-4 text-center transition-all duration-300 hover:my-0 hover:border-2 hover:pb-10 hover:pt-6 hover:shadow-md">
-        <Link
-          href=""
-          className="z-10 no-underline before:absolute before:left-0 before:top-0 before:block before:h-full before:w-full"
-        >
+    <div className="relative max-w-prose rounded-xl border-2 border-white px-10 py-8 text-white">
+      <Link
+        href={href}
+        className="w-full no-underline before:absolute before:left-0 before:top-0 before:block before:h-full before:w-full"
+      >
+        <div className="mb-5 flex items-center justify-between before:absolute before:left-0 before:top-0 before:block before:h-full before:w-full hover:underline">
           <h3>{title}</h3>
-          <p>-{'>'}</p>
-        </Link>
-      </div>
+          <ArrowRight className="h-8" />
+        </div>
+        <p>{content}</p>
+      </Link>
     </div>
   );
 };
--- docs/src/app/page.tsx
+++ docs/src/app/page.tsx
@@ -11,15 +11,34 @@ export default function FrontPage() {
   return (
     <div className="container min-h-screen py-14">
       <Image className="h-10 w-40" aria-hidden {...OBOS_LOGO} alt="" />
-      <div className="flex pt-20">
+      <div className="mt-20 flex">
         <h1 className="text-[116px] leading-none text-white">Grunnmuren </h1>
         <div className="bg-sky-lightest h-fit rounded-sm px-3 py-1.5 ">
           <p>Beta</p>
         </div>
       </div>
-      <h2 className="pt-4 font-normal text-white">OBOS' designsystem</h2>
-      <div className="grid grid-cols-2">
-        <Card title="Figma" content="Tester dette innholdet." />
+      <h2 className="mt-4 font-normal text-white">OBOS{"'"} designsystem</h2>
+      <div className="mt-20 grid grid-cols-2 gap-10">
+        <Card
+          title="Storybook"
+          content="På Storybook finner du alle komponentene med tilhørende eksempelkode."
+          href=""
+        />
+        <Card
+          title="Confluence"
+          content="På Confluence finner du dokumentasjon og retningslinjer for bruk."
+          href=""
+        />
+        <Card
+          title="Figma"
+          href="https://www.figma.com/file/XRHRRytz9DqrDkWpE4IKVB/OBOS-DS?type=design&node-id=221-746&mode=design&t=99r84414pa6dHvKV-0"
+          content="På Figma finner du designskisser på eksisterende og kommende komponenter."
+        />
+        <Card
+          title="Github"
+          href="https://github.com/code-obos/grunnmuren/tree/v2"
+          content="På Github finner du kildekoden til designsystemet."
+        />
       </div>
     </div>
   );
