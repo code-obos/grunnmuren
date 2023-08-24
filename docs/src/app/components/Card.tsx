30347827ec83386ef867105038f3b6d3f8ba37c6
Håkon Drangsland <46884914+haakondrang@users.noreply.github.com>
2023-08-24 10:27:58 +0200

    Add icons page and mobile responsiveness

--- docs/src/app/components/Card.tsx
+++ docs/src/app/components/Card.tsx
@@ -1,23 +1,32 @@
 import Link from 'next/link';
 import { FC } from 'react';
 import { ArrowRight } from '@obosbbl/grunnmuren-icons-react';
+import clsx from 'clsx';
 
 interface Props {
   title: string;
   content: string;
   href: string;
+  bgColor: 'blue' | 'white';
 }
 
-const Card: FC<Props> = ({ title, content, href }) => {
+const Card: FC<Props> = ({ title, content, href, bgColor }) => {
   return (
-    <div className="relative max-w-prose rounded-xl border-2 border-white px-10 py-8 text-white">
+    <div
+      className={clsx('relative max-w-prose rounded-xl px-10 py-8', {
+        'bg-white': bgColor === 'white',
+        'border-2 border-white text-white': bgColor === 'blue',
+      })}
+    >
       <Link
         href={href}
         className="w-full no-underline before:absolute before:left-0 before:top-0 before:block before:h-full before:w-full"
       >
-        <div className="mb-5 flex items-center justify-between before:absolute before:left-0 before:top-0 before:block before:h-full before:w-full hover:underline">
+        <div className="mb-3 flex items-center justify-between before:absolute before:left-0 before:top-0 before:block before:h-full before:w-full hover:underline">
           <h3>{title}</h3>
-          <ArrowRight className="h-8" />
+          <ArrowRight
+            className={clsx('h-8', { 'text-green': bgColor === 'white' })}
+          />
         </div>
         <p>{content}</p>
       </Link>
--- docs/src/app/page.tsx
+++ docs/src/app/page.tsx
@@ -12,32 +12,48 @@ export default function FrontPage() {
     <div className="container min-h-screen py-14">
       <Image className="h-10 w-40" aria-hidden {...OBOS_LOGO} alt="" />
       <div className="mt-20 flex">
-        <h1 className="text-[116px] leading-none text-white">Grunnmuren </h1>
-        <div className="bg-sky-lightest h-fit rounded-sm px-3 py-1.5 ">
-          <p>Beta</p>
+        <h1 className="text-[48px] leading-none text-white sm:text-[72px] lg:text-[116px]">
+          Grunnmuren{' '}
+        </h1>
+        <div className="h-fit rounded-sm bg-sky-lightest px-1 sm:px-3 sm:py-1.5 ">
+          <p className="text-[12px] sm:text-base">Beta</p>
         </div>
       </div>
-      <h2 className="mt-4 font-normal text-white">OBOS{"'"} designsystem</h2>
-      <div className="mt-20 grid grid-cols-2 gap-10">
-        <Card
-          title="Storybook"
-          content="På Storybook finner du alle komponentene med tilhørende eksempelkode."
-          href=""
-        />
+      <h2 className="h3 mt-4 font-normal text-white sm:h2 sm:font-normal">
+        OBOS{"'"} designsystem
+      </h2>
+      <div className="mb-10 mt-20 grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3">
         <Card
           title="Confluence"
           content="På Confluence finner du dokumentasjon og retningslinjer for bruk."
           href=""
+          bgColor="blue"
         />
         <Card
           title="Figma"
           href="https://www.figma.com/file/XRHRRytz9DqrDkWpE4IKVB/OBOS-DS?type=design&node-id=221-746&mode=design&t=99r84414pa6dHvKV-0"
           content="På Figma finner du designskisser på eksisterende og kommende komponenter."
+          bgColor="blue"
         />
         <Card
           title="Github"
           href="https://github.com/code-obos/grunnmuren/tree/v2"
           content="På Github finner du kildekoden til designsystemet."
+          bgColor="blue"
+        />
+      </div>
+      <div className="grid grid-cols-1 gap-10 sm:grid-cols-2">
+        <Card
+          title="Komponenter"
+          href=""
+          content="Komponentene og tilhørende eksempelkode ligger på Storybook."
+          bgColor="white"
+        />
+        <Card
+          title="Ikoner"
+          content="Alle ikonene i Grunnmuren ligger på denne siden."
+          href="/ikoner"
+          bgColor="white"
         />
       </div>
     </div>
