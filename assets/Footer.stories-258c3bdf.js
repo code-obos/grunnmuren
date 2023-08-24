1492ae1968c68e5ea570d2d8d15f66a74f06b081
aulonm <aulon.mujaj@gmail.com>
2023-08-24 06:28:08 +0000

    Deploying to docs from @ code-obos/grunnmuren@1e9939c217a08510326b35d740cf6d0368597478 🚀

---
+++ assets/Footer.stories-258c3bdf.js
@@ -0,0 +1,9 @@
+'use client';
+import{j as e}from"./jsx-runtime-7c577f00.js";import{v as a}from"./TextField-a1f4d5d5.js";import"./index-56a930d9.js";import"./clsx-388a5213.js";import"./icons-f7fcbcf7.js";import"./Alert-4d742b15.js";import"./ButtonColorContext-4a053ecd.js";import"./index-e979ca2b.js";const h={title:"Footer"},r=()=>e.jsx(a,{children:e.jsxs("div",{children:["I am mostly empty footer. ",e.jsx("a",{href:"#",children:"This link leads no where."})]})});var o,t,s;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`() => {
+  return <Footer>
+      <div>
+        I am mostly empty footer. <a href="#">This link leads no where.</a>
+      </div>
+    </Footer>;
+}`,...(s=(t=r.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,h as default};
+//# sourceMappingURL=Footer.stories-258c3bdf.js.map
---
+++ assets/WithTooltip-YBG737T3-2b2cf6f7.js
@@ -0,0 +1,3 @@
+'use client';
+import{W as m,W,a as T}from"./DocsRenderer-3PUGWF3O-03fef595.js";import"./iframe-41d4c108.js";import"../sb-preview/runtime.js";import"./index-56a930d9.js";import"./_getPrototype-4742e203.js";export{m as WithToolTipState,W as WithTooltip,T as WithTooltipPure};
+//# sourceMappingURL=WithTooltip-YBG737T3-2b2cf6f7.js.map
