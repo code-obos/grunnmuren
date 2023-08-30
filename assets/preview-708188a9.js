73b501c7e19d335ad9e71532b1d8b60a0e72f5c9
aulonm <aulon.mujaj@gmail.com>
2023-08-30 06:57:03 +0000

    Deploying to docs from @ code-obos/grunnmuren@b34223954add6bb1d747d441464f448b304a174d 🚀

--- assets/preview-708188a9.js
+++
@@ -1,3 +0,0 @@
-'use client';
-import{_ as t}from"./iframe-6cb2d8a3.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-3PUGWF3O-c4917221.js").then(e=>e.D),["./DocsRenderer-3PUGWF3O-c4917221.js","./iframe-6cb2d8a3.js","./index-56a930d9.js","./_getPrototype-4742e203.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-708188a9.js.map
