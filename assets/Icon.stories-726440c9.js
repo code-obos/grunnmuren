469cea8ccae323264b9c4a85197f1a81c9b7ad0f
aulonm <aulon.mujaj@gmail.com>
2023-09-02 12:31:55 +0000

    Deploying to docs from @ code-obos/grunnmuren@cfa7c6942105762a45b341dbc6c7129bde4ac164 🚀

---
+++ assets/Icon.stories-726440c9.js
@@ -0,0 +1,10 @@
+'use client';
+import{j as e}from"./jsx-runtime-7c577f00.js";import{i as o}from"./icons-2eb78a74.js";import"./index-56a930d9.js";const d={title:"Icons",parameters:{layout:"padded"}};function s(){return e.jsx("div",{className:"grid grid-cols-[repeat(auto-fill,_100px)] content-center justify-center gap-6",children:Object.entries(o).map(([t,c])=>e.jsxs("div",{children:[e.jsx(c,{className:"mx-auto mb-2"}),e.jsx("span",{className:"block text-center text-sm",children:t})]},t))})}var n,r,a;s.parameters={...s.parameters,docs:{...(n=s.parameters)==null?void 0:n.docs,source:{originalSource:`function Icons() {
+  return <div className="grid grid-cols-[repeat(auto-fill,_100px)] content-center justify-center gap-6">
+      {Object.entries(icons).map(([iconName, Icon]) => <div key={iconName}>
+          <Icon className="mx-auto mb-2" />
+          <span className="block text-center text-sm">{iconName}</span>
+        </div>)}
+    </div>;
+}`,...(a=(r=s.parameters)==null?void 0:r.docs)==null?void 0:a.source}}};const l=["Icons"];export{s as Icons,l as __namedExportsOrder,d as default};
+//# sourceMappingURL=Icon.stories-726440c9.js.map
---
+++ assets/preview-3b37ce70.js
@@ -0,0 +1,3 @@
+'use client';
+import{_ as t}from"./iframe-45674469.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-3PUGWF3O-a732436c.js").then(e=>e.D),["./DocsRenderer-3PUGWF3O-a732436c.js","./iframe-45674469.js","./index-56a930d9.js","./_getPrototype-4742e203.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-3b37ce70.js.map
