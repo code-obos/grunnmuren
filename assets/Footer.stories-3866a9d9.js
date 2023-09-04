989de5616c0e1a50d85fa51e754b3405a333f220
ingara <ingara@gmail.com>
2023-09-04 08:16:38 +0000

    Deploying to docs from @ code-obos/grunnmuren@f50be58f1f401a74ceb9a13cd757f565870bd246 🚀

--- assets/Footer.stories-3866a9d9.js
+++
@@ -1,9 +0,0 @@
-'use client';
-import{j as e}from"./jsx-runtime-7f3d8b90.js";import{v as a}from"./TextField-b006f993.js";import"./index-68775723.js";import"./clsx-388a5213.js";import"./icons-d96bec2d.js";import"./Alert-988c286c.js";import"./ButtonColorContext-f5c9c60b.js";import"./index-9353d67f.js";const h={title:"Footer"},r=()=>e.jsx(a,{children:e.jsxs("div",{children:["I am mostly empty footer. ",e.jsx("a",{href:"#",children:"This link leads no where."})]})});var o,t,s;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`() => {
-  return <Footer>
-      <div>
-        I am mostly empty footer. <a href="#">This link leads no where.</a>
-      </div>
-    </Footer>;
-}`,...(s=(t=r.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,h as default};
-//# sourceMappingURL=Footer.stories-3866a9d9.js.map
--- assets/preview-ef585f23.js
+++
@@ -1,3 +0,0 @@
-'use client';
-import{_ as t}from"./iframe-6eff6d01.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-3PUGWF3O-e0c87213.js").then(e=>e.D),["./DocsRenderer-3PUGWF3O-e0c87213.js","./iframe-6eff6d01.js","./index-68775723.js","./_getPrototype-9dc0159a.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-ef585f23.js.map
