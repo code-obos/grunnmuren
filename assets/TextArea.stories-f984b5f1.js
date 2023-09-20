5ff7b9d953a040807e95346b045e5954fb22f5b6
alexanbj <alexander.bjerkan@gmail.com>
2023-09-20 11:06:15 +0000

    Deploying to docs from @ code-obos/grunnmuren@3c2ef489c81fec23b7dfae8916c6c4bac7225b98 🚀

---
+++ assets/TextArea.stories-f984b5f1.js
@@ -0,0 +1,9 @@
+'use client';
+import{j as r}from"./jsx-runtime-7f3d8b90.js";import{a}from"./TextField-ab4880b9.js";import"./Alert-7c0cfde0.js";import"./ButtonColorContext-7a5ab71d.js";import"./index-9353d67f.js";import"./index-68775723.js";import"./clsx-388a5213.js";import"./icons-4ce3ec7c.js";const u={title:"TextArea",parameters:{layout:"padded"}},e=()=>r.jsxs("div",{className:"flex flex-col gap-4",children:[r.jsx(a,{label:"Har du noen tilbakemeldinger?",maxLength:200,description:"Maks 200 tegn"}),r.jsx(a,{label:"Har du noen tilbakemeldinger?",maxLength:200,description:"Maks 200 tegn",required:!0,error:"Påkrevd"})]});var t,n,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`() => {
+  return <div className="flex flex-col gap-4">
+      <TextArea label="Har du noen tilbakemeldinger?" maxLength={200} description="Maks 200 tegn" />
+
+      <TextArea label="Har du noen tilbakemeldinger?" maxLength={200} description="Maks 200 tegn" required error="Påkrevd" />
+    </div>;
+}`,...(o=(n=e.parameters)==null?void 0:n.docs)==null?void 0:o.source}}};const g=["Default"];export{e as Default,g as __namedExportsOrder,u as default};
+//# sourceMappingURL=TextArea.stories-f984b5f1.js.map
--- assets/syntaxhighlighter-PONEFOSF-35ae13b9.js
+++
@@ -1,3 +0,0 @@
-'use client';
-import{S as e,c as m,s}from"./DocsRenderer-3PUGWF3O-76675f40.js";import"./iframe-711be68b.js";import"../sb-preview/runtime.js";import"./index-68775723.js";import"./_getPrototype-9dc0159a.js";export{e as SyntaxHighlighter,m as createCopyToClipboardFunction,s as default};
-//# sourceMappingURL=syntaxhighlighter-PONEFOSF-35ae13b9.js.map
