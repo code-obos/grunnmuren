8c1786147621ece9bb2d9dcdd66b025964169856
Alexander Bjerkan <alexander.bjerkan@obos.no>
2023-11-06 12:57:14 +0100

    v2 label design changes
    remove asterix

--- packages/react/src/label/Label.tsx
+++ packages/react/src/label/Label.tsx
@@ -4,33 +4,16 @@ import { Label as RACLabel } from 'react-aria-components';
 type LabelProps = {
   children: React.ReactNode;
   className?: string;
-  /**
-   * A visual indicator for the label that user input is required on the input before form submission.
-   * @default false
-   */
-  isRequired?: boolean;
-  /**
-   * Display the visual indicator for required fields as invalid. Only has an effect if `isRequired` is `true`.
-   * @default false
-   */
-  isInvalid?: boolean;
 };
 
 function Label(props: LabelProps) {
-  const { children, className, isInvalid, isRequired, ...restProps } = props;
+  const { children, className, ...restProps } = props;
 
   return (
-    <RACLabel className={cx(className, 'font-semibold')} {...restProps}>
-      {isRequired && (
-        <span
-          className={cx(
-            'mr-1 select-none',
-            isInvalid ? 'text-red' : 'text-blue',
-          )}
-        >
-          *
-        </span>
-      )}
+    <RACLabel
+      className={cx(className, 'font-semibold leading-7')}
+      {...restProps}
+    >
       {children}
     </RACLabel>
   );
