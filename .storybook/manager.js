efdc2767484e343f8db226b17751b828868560f6
Alexander Bjerkan <alexander.bjerkan@obos.no>
2023-11-08 10:42:48 +0100

    Add Checkbox and CheckboxGroup to v2 (#617)

---
+++ .storybook/manager.js
@@ -0,0 +1,9 @@
+import { addons } from '@storybook/manager-api';
+
+// Allows us to place CheckboxGroup under Checkbox as a folder instead of a new root
+// https://storybook.js.org/docs/react/configure/sidebar-and-urls#roots
+addons.setConfig({
+  sidebar: {
+    showRoots: false,
+  },
+});
