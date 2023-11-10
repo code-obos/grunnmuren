794b30440488ba7dbf04a4a3f40a24f078c3c02a
alexanbj <alexander.bjerkan@gmail.com>
2023-11-10 12:41:00 +0000

    Deploying to docs from @ code-obos/grunnmuren@bf8a694e85a1cda3beea98c30053556f277e02d9 🚀

---
+++ assets/preview-24fb2c0c.js
@@ -0,0 +1,3 @@
+'use client';
+const o={layout:"fullscreen"};export{o as parameters};
+//# sourceMappingURL=preview-24fb2c0c.js.map
--- assets/preview-339a719d.js
+++
@@ -1,3 +0,0 @@
-'use client';
-const o={layout:"fullscreen"};export{o as parameters};
-//# sourceMappingURL=preview-339a719d.js.map
