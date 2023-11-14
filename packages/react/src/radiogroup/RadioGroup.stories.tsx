1f0c39ff19964a8e700ee1bb897d9438855c57cc
Alexander Bjerkan <alexander.bjerkan@obos.no>
2023-11-14 14:22:12 +0100

    flesh out storybook stories

--- packages/react/src/radiogroup/RadioGroup.stories.tsx
+++ packages/react/src/radiogroup/RadioGroup.stories.tsx
@@ -1,37 +1,133 @@
+import { useState, useEffect } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { RadioGroup } from './RadioGroup';
+import { Button } from '../button/Button';
+import { RadioGroup, type RadioGroupProps } from './RadioGroup';
 import { Radio } from './Radio';
 
 const meta: Meta<typeof RadioGroup> = {
   title: 'RadioGroup',
   component: RadioGroup,
-  render: (args) => (
-    <RadioGroup {...args}>
-      <Radio value="ordinary">Ordinær pris</Radio>
-      <Radio value="bostart">OBOS Bostart</Radio>
-      <Radio value="deleie">OBOS Deleie</Radio>
-    </RadioGroup>
-  ),
+  argTypes: {
+    onChange: { action: 'changed' },
+  },
 };
 
 export default meta;
 
 type Story = StoryObj<typeof RadioGroup>;
 
-export const Example: Story = {
+const items = (
+  <>
+    <Radio value="ordinary">Ordinær pris</Radio>
+    <Radio value="bostart">OBOS Bostart</Radio>
+    <Radio value="deleie">OBOS Deleie</Radio>
+  </>
+);
+
+const Template = (args: RadioGroupProps) => {
+  return args.isRequired ? (
+    <form
+      className="flex flex-col items-start gap-4"
+      onSubmit={(e) => {
+        e.preventDefault();
+        alert('Lagret!');
+      }}
+    >
+      <RadioGroup {...args}>{items}</RadioGroup>
+      <Button type="submit">Send inn</Button>
+    </form>
+  ) : (
+    <RadioGroup {...args}>{items}</RadioGroup>
+  );
+};
+
+const ControlledTemplate = (args: RadioGroupProps) => {
+  const [selectedItem, setSelectedItem] = useState<string>('ordinary');
+
+  useEffect(() => {
+    console.log('isSelected:', selectedItem);
+  }, [selectedItem]);
+
+  return (
+    <div className="flex flex-col gap-2">
+      <RadioGroup
+        label="Select city"
+        value={selectedItem}
+        {...args}
+        onChange={setSelectedItem}
+      >
+        {items}
+      </RadioGroup>
+      <p className="text-default-500">Valgt: {selectedItem}</p>
+    </div>
+  );
+};
+
+const defaultProps = {
+  label: 'Velg hvordan du vil kjøpe boligen',
+  isRequired: false,
+  isInvalid: false,
+  defaultValue: undefined,
+};
+
+export const Default: Story = {
+  render: Template,
   args: {
-    description:
-      'Kontakt oss for mer utfyllende informasjon om de forskjellige alternativene',
-    label: 'Velg hvordan du vil kjøpe boligen',
-    isRequired: true,
-    isInvalid: false,
+    ...defaultProps,
   },
 };
 
-export const AsInvalid: Story = {
+export const DefaultChecked: Story = {
+  render: Template,
   args: {
-    ...Example.args,
-    errorMessage: 'Velg et alternativ for å fortsette',
+    ...defaultProps,
+    defaultValue: 'deleie',
+  },
+};
+
+export const isRequired: Story = {
+  render: Template,
+
+  args: {
+    ...defaultProps,
+    isRequired: true,
+  },
+};
+
+export const WithDescription: Story = {
+  render: Template,
+
+  args: {
+    ...defaultProps,
+    description:
+      'Kontakt oss for mer utfyllende informasjon om de forskjellige alternativene',
+  },
+};
+
+export const IsInvalid: Story = {
+  render: Template,
+
+  args: {
+    ...defaultProps,
+    isInvalid: true,
+  },
+};
+
+export const WithErrorMessage: Story = {
+  render: Template,
+
+  args: {
+    ...defaultProps,
+    isInvalid: true,
+    errorMessage: 'Det valgte alternativet er ikke gyldig',
+  },
+};
+
+export const Controlled = {
+  render: ControlledTemplate,
+
+  args: {
+    ...defaultProps,
   },
 };
