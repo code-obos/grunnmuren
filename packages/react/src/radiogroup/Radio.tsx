d1df772e695ef3e8e7a7103d91c574525b06f4b7
Alexander Bjerkan <alexander.bjerkan@obos.no>
2023-11-14 18:45:32 +0100

    done with the hover states

--- packages/react/src/radiogroup/Radio.tsx
+++ packages/react/src/radiogroup/Radio.tsx
@@ -10,13 +10,13 @@ const defaultClasses = cx([
   'before:h-6 before:w-6 before:flex-none before:rounded-full before:border-2 before:border-black',
   // selected
   'data-[selected]:before:border-black data-[selected]:before:bg-green data-[selected]:before:shadow-[inset_0_0_0_4px_rgb(255,255,255)]',
-  // hover states
-  'before:bg-green-lightest data-[hovered]:before:border-green data-[hovered]:before:bg-green-lightest',
+  // hover
+  'data-[hovered]:before:border-green data-[hovered]:before:bg-green-lightest data-[hovered]:data-[invalid]:before:bg-red-light',
   // focus
   'data-[focus-visible]:before:ring data-[focus-visible]:before:ring-black data-[focus-visible]:before:ring-offset-[9px]',
   // invalid - The border is 1 px thicker when invalid. We don't actually want to change the border width, as that causes the element's size to change
   // so we use an inner outline to artifically pad the border
-  'data-[invalid]:before:outline-solid data-[invalid]:before:!border-red data-[invalid]:data-[selected]:before:bg-red data-[invalid]:before:outline data-[invalid]:before:outline-[3px] data-[invalid]:before:outline-offset-[-3px] data-[invalid]:before:outline-red',
+  'data-[invalid]:before:outline-solid data-[invalid]:before:border-red data-[invalid]:data-[selected]:before:!bg-red data-[invalid]:before:outline data-[invalid]:before:outline-[3px] data-[invalid]:before:outline-offset-[-3px] data-[invalid]:before:outline-red',
 ]);
 
 type RadioProps = {
