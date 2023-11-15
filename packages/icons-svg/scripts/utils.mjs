6e1ade92711a11514611e750d8f055076cea29c0
Alexander Bjerkan <alexander.bjerkan@obos.no>
2023-11-15 13:14:44 +0100

    import icons from v2 Figma

--- packages/icons-svg/scripts/utils.mjs
+++ packages/icons-svg/scripts/utils.mjs
@@ -1,12 +1,12 @@
-import fs from 'fs-extra';
 import path from 'path';
+import { globSync } from 'glob';
 import { fileURLToPath } from 'url';
 
 export const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // Assumes the folder only contains SVGs. Poor man's glob
-export function listSvgs(dirPath) {
-  const files = fs.readdirSync(dirPath);
+export function listSvgs() {
+  const files = globSync('raw/**/*.svg', { absolute: true });
 
-  return files.map((file) => path.join(dirPath, file));
+  return files;
 }
