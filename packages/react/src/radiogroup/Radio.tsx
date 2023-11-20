bc5a35bce7c5be8db1232f0155d86682c569ae7e
Alexander Bjerkan <alexander.bjerkan@obos.no>
2023-11-20 13:28:39 +0100

    add description support to radio buttons (#643)

--- packages/react/src/radiogroup/Radio.tsx
+++ packages/react/src/radiogroup/Radio.tsx
@@ -4,10 +4,17 @@ import {
   RadioProps as RACRAdioProps,
 } from 'react-aria-components';
 
+import { Description } from '../label';
+
 const defaultClasses = cx([
-  'relative inline-flex max-w-fit cursor-pointer items-center gap-4 py-2 leading-7',
+  'relative inline-flex max-w-fit cursor-pointer items-start gap-4 py-2 leading-7',
   // the radio button itself
-  'before:h-6 before:w-6 before:flex-none before:rounded-full before:border-2 before:border-black',
+  'before:flex-none before:rounded-full before:border-2 before:border-black',
+  // to vertically align the radio we need to calculate the label's height, which is equal to it's font size multiplied by the line height.
+  // For the ::before psuedo element the line height of the label is always 1em.
+  // When we know the height of the label we use the height of the radio to push it down to align with the label's first line
+  // TODO: 1.75 here is the unit less lineheight, altough we use 1.75rem as the line height, so there is a mismatch here. Revisit this when we've worked on typography in v2. Should this be a CSS custom property instead?
+  'before:mt-[calc((1em_*_1.75_-_24px)_/_2)] before:h-[24px] before:w-[24px]',
   // selected
   'data-[selected]:before:border-black data-[selected]:before:bg-green data-[selected]:before:shadow-[inset_0_0_0_4px_rgb(255,255,255)]',
   // hover
@@ -21,16 +28,27 @@ const defaultClasses = cx([
 
 type RadioProps = {
   children: React.ReactNode;
+  /** Additional CSS className for the element. */
   className?: string;
+  /** Help text for the form control. */
+  description?: React.ReactNode;
+  /** Additional style properties for the element. */
+  style?: React.CSSProperties;
 } & Omit<RACRAdioProps, 'isDisabled' | 'children' | 'style'>;
 
 function Radio(props: RadioProps) {
-  const { children, className, ...restProps } = props;
+  const { children, className, description, ...restProps } = props;
   return (
     <RACRadio {...restProps} className={cx(className, defaultClasses)}>
       {/* increases the clickable area of the radio button for accessibility */}
-      <div className="absolute -left-2.5 top-1/2 z-10 h-11 w-11 -translate-y-1/2" />
-      {children}
+      <div className="absolute -left-2.5 top-0 z-10 h-11 w-11 " />
+
+      <div>
+        {children}
+        {description && (
+          <Description className="mt-2.5 block">{description}</Description>
+        )}
+      </div>
     </RACRadio>
   );
 }
