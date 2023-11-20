fd83e0172b3d6fcea26308de3e94119ff3b4c1ab
Alexander Bjerkan <alexander.bjerkan@obos.no>
2023-11-20 13:26:29 +0100

    add TODO as per PR comment

--- packages/react/src/radiogroup/Radio.tsx
+++ packages/react/src/radiogroup/Radio.tsx
@@ -13,6 +13,7 @@ const defaultClasses = cx([
   // to vertically align the radio we need to calculate the label's height, which is equal to it's font size multiplied by the line height.
   // For the ::before psuedo element the line height of the label is always 1em.
   // When we know the height of the label we use the height of the radio to push it down to align with the label's first line
+  // TODO: 1.75 here is the unit less lineheight, altough we use 1.75rem as the line height, so there is a mismatch here. Revisit this when we've worked on typography in v2. Should this be a CSS custom property instead?
   'before:mt-[calc((1em_*_1.75_-_24px)_/_2)] before:h-[24px] before:w-[24px]',
   // selected
   'data-[selected]:before:border-black data-[selected]:before:bg-green data-[selected]:before:shadow-[inset_0_0_0_4px_rgb(255,255,255)]',
