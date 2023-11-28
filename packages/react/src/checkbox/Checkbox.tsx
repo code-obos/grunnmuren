b15f72badcd57bcfd3f9984077c3739b5aa3e44c
Alexander Bjerkan <alexander.bjerkan@obos.no>
2023-11-28 12:53:34 +0100

    v2: Add description and errormessage support to Checkbox (#651)

--- packages/react/src/checkbox/Checkbox.tsx
+++ packages/react/src/checkbox/Checkbox.tsx
@@ -1,12 +1,17 @@
+import { useId } from 'react';
 import { cx } from 'cva';
 import {
+  CheckboxContext,
   Checkbox as RACCheckbox,
   CheckboxProps as RACCheckboxProps,
 } from 'react-aria-components';
 import { Check as CheckIcon } from '@obosbbl/grunnmuren-icons-react';
 
+import { ErrorMessage } from '../label/ErrorMessage';
+import { Description } from '../label/Description';
+
 const defaultClasses = cx([
-  'group relative inline-flex max-w-fit cursor-pointer items-center gap-4 py-2 leading-7',
+  'group relative left-0 inline-flex max-w-fit cursor-pointer items-start gap-4 py-2 leading-7',
 ]);
 
 // Pulling this out into it's own component. Will probably export it in the future
@@ -15,7 +20,12 @@ function CheckmarkBox() {
   return (
     <div
       className={cx([
-        'relative grid h-6 w-6 flex-none place-content-center rounded-sm border-2 border-black text-white',
+        'relative left-0 grid flex-none place-content-center rounded-sm border-2 border-black text-white',
+        // to vertically align the radio we need to calculate the label's height, which is equal to it's font size multiplied by the line height.
+        // For the ::before psuedo element the line height of the label is always 1em.
+        // When we know the height of the label we use the height of the radio to push it down to align with the label's first line
+        // TODO: 1.75 here is the unit less lineheight, altough we use 1.75rem as the line height, so there is a mismatch here. Revisit this when we've worked on typography in v2. Should this be a CSS custom property instead?
+        'mt-[calc((1em_*_1.75_-_24px)_/_2)] h-[24px] w-[24px]',
         // selected
         'group-data-[selected]:!border-green group-data-[selected]:!bg-green',
         // focus
@@ -34,21 +44,67 @@ function CheckmarkBox() {
 
 type CheckboxProps = {
   children: React.ReactNode;
+  /** Additional CSS className for the element. */
   className?: string;
+  /** Help text for the form control. */
+  description?: React.ReactNode;
+  /** Error message for the form control. Automatically sets `isInvalid` to true */
+  errorMessage?: React.ReactNode;
+  /** Additional style properties for the element. */
+  style?: React.CSSProperties;
 } & Omit<
   RACCheckboxProps,
   'isDisabled' | 'style' | 'children' | 'isIndeterminate' | 'isReadOnly'
 >;
 
 function Checkbox(props: CheckboxProps) {
-  const { children, className, ...restProps } = props;
+  const {
+    children,
+    className,
+    description,
+    errorMessage,
+    isInvalid: _isInvalid,
+    ...restProps
+  } = props;
+
+  const id = useId();
+
+  const descriptionId = 'desc' + id;
+  const errorMessageId = 'error' + id;
+
+  const isInvalid = _isInvalid || errorMessage != null;
+
   return (
-    <RACCheckbox {...restProps} className={cx(className, defaultClasses)}>
-      {/* increases the clickable area of the checkbox for accessibility */}
-      <div className="absolute -left-2.5 top-1/2 z-10 h-11 w-11 -translate-y-1/2" />
-      <CheckmarkBox />
-      {children}
-    </RACCheckbox>
+    <div>
+      <CheckboxContext.Provider
+        value={{
+          'aria-describedby': description ? descriptionId : undefined,
+          'aria-errormessage': errorMessage ? errorMessageId : undefined,
+        }}
+      >
+        <RACCheckbox
+          {...restProps}
+          className={cx(className, defaultClasses)}
+          isInvalid={isInvalid}
+        >
+          {/* increases the clickable area of the checkbox for accessibility */}
+          <div className="absolute -left-2.5 top-0 z-10 h-11 w-11" />
+          <CheckmarkBox />
+          {children}
+        </RACCheckbox>
+
+        {description && (
+          <Description className="block" id={descriptionId}>
+            {description}
+          </Description>
+        )}
+        {errorMessage && (
+          <ErrorMessage className="mt-2 block" id={errorMessageId}>
+            {errorMessage}
+          </ErrorMessage>
+        )}
+      </CheckboxContext.Provider>
+    </div>
   );
 }
 
--- packages/react/src/label/Description.tsx
+++ packages/react/src/label/Description.tsx
@@ -1,10 +1,7 @@
 import { cx } from 'cva';
-import { Text } from 'react-aria-components';
+import { Text, type TextProps } from 'react-aria-components';
 
-type DescriptionProps = {
-  className?: string;
-  children: React.ReactNode;
-};
+type DescriptionProps = TextProps;
 
 function Description(props: DescriptionProps) {
   const { className, ...restProps } = props;
