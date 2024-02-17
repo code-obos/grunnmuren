134a20742bf869dc1cc3bcee87e24b6a26af228f
Alexander Bjerkan <alexander.bjerkan@obos.no>
2024-02-17 11:09:36 +0100

    generate font fallback

--- packages/tailwind/font-fallback.ts
+++ packages/tailwind/font-fallback.ts
@@ -1,5 +1,5 @@
 import { Glob } from 'bun';
-import { readMetrics, generateFontFace, getMetricsForFamily } from 'fontaine';
+import { readMetrics, generateFontFace } from 'fontaine';
 
 const fontGlob = new Glob('*.woff2');
 
@@ -9,9 +9,41 @@ for await (const fontFile of fontGlob.scan({
 })) {
   const fontMetrics = await readMetrics('file://' + fontFile);
 
-  const fontFace = generateFontFace(fontMetrics, {
+  if (fontMetrics == null) {
+    throw new Error('Unable to read metrics for ', fontFile);
+  }
+  console.log(fontFile, fontMetrics);
+
+  /**
+  @font-face {
+    font-family: "__OBOSFont_Fallback";
+    src: local("Arial");
+    size-adjust: 100%;
+    ascent-override: 94%;
+    descent-override: 26%;
+    line-gap-override: 0%;
+  }
+   */
+  const fontFaceDeclaration = generateFontFace(fontMetrics, {
     name: '__OBOSFont_Fallback',
     font: 'Arial',
   });
-  console.log(fontFace);
+
+  const lines = fontFaceDeclaration.split('\n');
+
+  // Remove the first and last line, leaving us with the "rules"
+  // of the declaration instead of the declaration itself.
+  const rules = lines.slice(1, -2);
+
+  const obj = {};
+  for (const rule of rules) {
+    let [property, value] = rule.split(':').map((v) => v.trim());
+    value = value.slice(0, -1);
+
+    obj[property] = value;
+  }
+
+  console.log(obj);
+
+  await Bun.write('./fallback.js', `module.exports = ${JSON.stringify(obj)}`);
 }
