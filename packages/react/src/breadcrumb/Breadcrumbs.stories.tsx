fb1253589445479cf1ef4ee6e5baa7ef4d7f96bd
Gabriel Wallin <gabriel.wallin@bekk.no>
2024-03-01 14:56:19 +0100

    update stories with simpler api

--- packages/react/src/breadcrumb/Breadcrumbs.stories.tsx
+++ packages/react/src/breadcrumb/Breadcrumbs.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { Breadcrumbs } from '../breadcrumb';
+import { Breadcrumb } from './Breadcrumb';
 
 const meta: Meta<typeof Breadcrumbs> = {
   title: 'Breadcrumbs',
@@ -8,13 +9,11 @@ const meta: Meta<typeof Breadcrumbs> = {
 
   render: () => {
     return (
-      <Breadcrumbs
-        links={[
-          { href: '/', text: 'Min side' },
-          { href: '/', text: 'Instillinger' },
-          { href: '/', text: 'Kontaktinformasjon' },
-        ]}
-      />
+      <Breadcrumbs>
+        <Breadcrumb href="/">Min side</Breadcrumb>
+        <Breadcrumb href="/">Instillinger</Breadcrumb>
+        <Breadcrumb>Kontaktinformasjon</Breadcrumb>
+      </Breadcrumbs>
     );
   },
 };
@@ -25,10 +24,28 @@ type Story = StoryObj<typeof Breadcrumbs>;
 
 export const Default: Story = {
   args: {
-    links: [
-      { href: '/', text: 'Min side' },
-      { href: '/', text: 'Instillinger' },
-      { href: '/', text: 'Kontaktinformasjon' },
-    ],
+    children: (
+      <Breadcrumbs>
+        <Breadcrumb href="/">Min side</Breadcrumb>
+        <Breadcrumb href="/">Instillinger</Breadcrumb>
+        <Breadcrumb href="/">Kontaktinformasjon</Breadcrumb>
+      </Breadcrumbs>
+    ),
   },
+};
+
+export const Long = () => {
+  return (
+    <Breadcrumbs>
+      <Breadcrumb href="/">Min side</Breadcrumb>
+      <Breadcrumb href="/">Instillinger</Breadcrumb>
+      <Breadcrumb href="/">Kontaktinformasjon</Breadcrumb>
+      <Breadcrumb href="/">Side 1</Breadcrumb>
+      <Breadcrumb href="/">Side 2</Breadcrumb>
+      <Breadcrumb href="/">Side 3</Breadcrumb>
+      <Breadcrumb href="/">Side 4</Breadcrumb>
+      <Breadcrumb href="/">Side 5</Breadcrumb>
+      <Breadcrumb href="/">Side 6</Breadcrumb>
+    </Breadcrumbs>
+  );
 };
