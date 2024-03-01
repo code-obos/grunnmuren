a80442a3676b574cddf80668f1dd22a0cefbfe7c
Gabriel Wallin <gabriel.wallin@bekk.no>
2024-03-01 14:55:04 +0100

    Simplify breadcrumbs

--- packages/react/src/breadcrumb/Breadcrumbs.tsx
+++ packages/react/src/breadcrumb/Breadcrumbs.tsx
@@ -2,11 +2,9 @@ import { forwardRef } from 'react';
 import { cx } from 'cva';
 import {
   Breadcrumbs as RACBreadcrumbs,
-  Link as RACLink,
   type BreadcrumbsProps as RACBreadcrumbsProps,
 } from 'react-aria-components';
-import { ChevronRight } from '@obosbbl/grunnmuren-icons-react';
-import { Breadcrumb, BreadcrumbProps } from './Breadcrumb';
+import { BreadcrumbProps } from './Breadcrumb';
 
 const defaultClasses = 'flex flex-wrap';
 
@@ -16,26 +14,14 @@ type BreadcrumbsProps = {
 
   /** Additional style properties for the element. */
   style?: React.CSSProperties;
-  links: Array<{ href: string; text: string }>;
 } & Omit<RACBreadcrumbsProps<BreadcrumbProps>, 'className' | 'style'>;
 
 function Breadcrumbs(props: BreadcrumbsProps) {
-  const { className, links, ...restProps } = props;
+  const { className, children, ...restProps } = props;
 
   return (
     <RACBreadcrumbs {...restProps} className={cx(className, defaultClasses)}>
-      {links.map((link, i) => (
-        <Breadcrumb key={i} className="flex items-center">
-          {i === links.length - 1 ? (
-            <p>{link.text}</p>
-          ) : (
-            <>
-              <RACLink href={link.href}>{link.text}</RACLink>
-              <ChevronRight className="px-1" />
-            </>
-          )}
-        </Breadcrumb>
-      ))}
+      {children}
     </RACBreadcrumbs>
   );
 }
