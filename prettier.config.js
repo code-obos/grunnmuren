e3329e075cb0db7578d110fed51535aaee8a3c6d
Alexander Bjerkan <alexander.bjerkan@obos.no>
2024-03-04 18:20:14 +0100

    prettier config as mjs

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,8 @@
-module.exports = {
+/** @type {import("prettier").Config} */
+const config = {
   singleQuote: true,
   plugins: ['prettier-plugin-tailwindcss', 'prettier-plugin-packagejson'],
   tailwindFunctions: ['cx', 'cva'],
 };
+
+export default config;
