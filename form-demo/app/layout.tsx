483b29a5b0b4dcf76767287af5faa7002657c312
Gabriel Wallin <gabriel.wallin@bekk.no>
2024-03-05 14:27:24 +0100

    remove example

--- form-demo/app/layout.tsx
+++ form-demo/app/layout.tsx
@@ -1,5 +1,5 @@
+import { GrunnmurenProvider } from '@obosbbl/grunnmuren-react';
 import './globals.css';
-import { Providers } from './providers';
 
 export const metadata = {
   title: 'Grunnmuren form examples',
@@ -13,12 +13,12 @@ export default function RootLayout({
   const locale = 'nb';
 
   return (
-    <Providers locale={locale}>
+    <GrunnmurenProvider locale={locale}>
       <html lang={locale}>
         <body>
           <main className="container-prose">{children}</main>
         </body>
       </html>
-    </Providers>
+    </GrunnmurenProvider>
   );
 }
--- form-demo/app/uncontrolled/page.tsx
+++ form-demo/app/uncontrolled/page.tsx
@@ -1,11 +1,6 @@
 'use client';
 import { useFormState } from 'react-dom';
-import {
-  Breadcrumb,
-  Breadcrumbs,
-  Form,
-  TextField,
-} from '@obosbbl/grunnmuren-react';
+import { Form, TextField } from '@obosbbl/grunnmuren-react';
 
 import { submitForm } from './submitAction';
 import SubmitButton from './SubmitButton';
@@ -14,30 +9,24 @@ export default function () {
   const [{ errors }, formAction] = useFormState(submitForm, { errors: {} });
 
   return (
-    <>
-      <Breadcrumbs>
-        <Breadcrumb href="/">Home</Breadcrumb>
-        <Breadcrumb href="/uncontrolled">Uncontrolled</Breadcrumb>
-      </Breadcrumbs>
-      <Form action={formAction} validationErrors={errors} className="space-y-4">
-        <p>
-          This is an uncontrolled form that uses zod to validate the form data
-          on the server side in a React server action.
-        </p>
-        <TextField name="name" label="Name" isRequired />
-        <TextField
-          name="email"
-          label="Email"
-          type="email"
-          isRequired
-          description={
-            <>
-              Must be a <em>.no</em> email address
-            </>
-          }
-        />
-        <SubmitButton>Save</SubmitButton>
-      </Form>
-    </>
+    <Form action={formAction} validationErrors={errors} className="space-y-4">
+      <p>
+        This is an uncontrolled form that uses zod to validate the form data on
+        the server side in a React server action.
+      </p>
+      <TextField name="name" label="Name" isRequired />
+      <TextField
+        name="email"
+        label="Email"
+        type="email"
+        isRequired
+        description={
+          <>
+            Must be a <em>.no</em> email address
+          </>
+        }
+      />
+      <SubmitButton>Save</SubmitButton>
+    </Form>
   );
 }
