5f5cb21b09287984dfbcddf74afd21d1a499b833
Gabriel Wallin <gabriel.wallin@bekk.no>
2024-03-15 10:28:37 +0100

    Add stories for backlink

---
+++ packages/react/src/backlink/Backlink.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta } from '@storybook/react';
+
+import { Backlink } from '.';
+
+const meta: Meta<typeof Backlink> = {
+  title: 'Backlinks',
+  component: Backlink,
+};
+
+export default meta;
+
+export const Default = () => {
+  return <Backlink href="#">Tillbake</Backlink>;
+};
+
+export const WithCustomIcon = () => {
+  return (
+    <Backlink href="#" iconStyle="text-green">
+      Tillbake
+    </Backlink>
+  );
+};
