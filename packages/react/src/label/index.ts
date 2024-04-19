a8a57c84a9aee5061c6d4748504224f5e2f5a00f
Oscar Carlström <oscar.carlstrom@gmail.com>
2024-04-19 08:40:48 +0200

    Replace Description with div to remove extra wrapper div

--- packages/react/src/label/index.ts
+++ packages/react/src/label/index.ts
@@ -1,3 +1,7 @@
-export { Description, type DescriptionProps } from './Description';
+export {
+  Description,
+  type DescriptionProps,
+  descriptionClasses,
+} from './Description';
 export { ErrorMessage, type ErrorMessageProps } from './ErrorMessage';
 export { Label, type LabelProps } from './Label';
