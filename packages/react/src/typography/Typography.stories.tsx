0b12f10a1f47f1d564f9391a17b265be2f4de77d
Oscar Carlström <oscar.carlstrom@gmail.com>
2024-05-14 13:28:51 +0200

    Add stories for Body

--- packages/react/src/typography/Typography.stories.tsx
+++ packages/react/src/typography/Typography.stories.tsx
@@ -1,6 +1,19 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { H1, H2, H3, H4, H5, H6, HeadingProps } from '.';
+import { Body, H1, H2, H3, H4, H5, H6, HeadingProps } from '.';
+
+const Template = (args: HeadingProps) => {
+  return (
+    <>
+      <H1 {...args}>Grunnmuren</H1>
+      <Body>Grunnmuren er designsystemet til OBOS.</Body>
+      <H2 {...args}>Typografi</H2>
+      <Body>
+        Typografi defineres av både tailwind-klasser og react-komponenter.
+      </Body>
+    </>
+  );
+};
 
 const HeadingsTemplate = (args: HeadingProps) => {
   return (
@@ -15,6 +28,24 @@ const HeadingsTemplate = (args: HeadingProps) => {
   );
 };
 
+const BodyTemplate = () => {
+  return (
+    <>
+      <Body>
+        Mange opplever at prisvekst og rentehevinger har fått store konsekvenser
+        for økonomien. Førstegangskjøpere og de som tjener mindre, sliter med å
+        komme seg inn på boligmarkedet.
+      </Body>
+      <Body>
+        – Det er et tøft boligmarked, og med de prisene det er på bolig i Oslo,
+        virket det nesten umulig å komme inn på markedet. Jeg var på noen
+        visninger, men det ble enten for lite eller for gammelt og slitt,
+        forteller boligkjøper Brita (30).
+      </Body>
+    </>
+  );
+};
+
 const meta: Meta = {
   title: 'Typography',
 };
@@ -23,7 +54,17 @@ export default meta;
 
 const defaultProps = {} as const;
 
+export const Default: StoryObj = {
+  render: Template,
+  args: defaultProps,
+};
+
 export const Headings: StoryObj = {
   render: HeadingsTemplate,
+  args: defaultProps,
+};
+
+export const Bodies: StoryObj = {
+  render: BodyTemplate,
   args: defaultProps,
 };
