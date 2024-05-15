f7a1889805ba81663d787f12410f4e4619bd754d
Oscar Carlström <oscar.carlstrom@gmail.com>
2024-05-15 13:28:07 +0200

    Update stories

--- packages/react/src/__stories__/Typography.stories.tsx
+++ packages/react/src/__stories__/Typography.stories.tsx
@@ -5,17 +5,35 @@ export const Default = () => {
     <div className="grid gap-8">
       <h1 className="h1">Grunnmuren</h1>
       <p className="lead">Grunnmuren er designsystemet til OBOS.</p>
-      <h2 className="h2">Typografi</h2>
-      <p className="body">Typografi defineres av tailwind-klasser.</p>
+      <h2 className="h2">Typografi i Grunnmuren</h2>
+      <p className="body">
+        Typografien i Grunnmuren defineres av tailwind-klasser. Denne teksten
+        har for eksempel klassen <code>body</code>.
+      </p>
+      <h3 className="h3">Sitater</h3>
+      <p className="body">
+        Lengre sitater kan framheves med klassen <code>blockquote</code>:
+      </p>
       <blockquote className="blockquote">
         Det er et tøft boligmarked, og med de prisene det er på bolig i Oslo,
         virket det nesten umulig å komme inn på markedet. Jeg var på noen
         visninger, men det ble enten for lite eller for gammelt og slitt
       </blockquote>
-      <figcaption className="description">
-        Lav møblering gjør at rommet oppleves større. Zen Milano-sengen er fra
-        Bohus, mens Soft Serve-lampen er fra Moniker.
-      </figcaption>
+      <h3 className="h3">Bildetekster</h3>
+      <p className="body">
+        Klassen <code>description</code> brukes for bildetekster:
+      </p>
+      <figure>
+        <img
+          className="mb-4 max-w-96 bg-blue-dark p-4"
+          src="https://res.cloudinary.com/obosit-prd-ch-clry/image/upload/q_auto/v1619689575/OBOS%20Merkevare/OBOS/Liggende/obos_liggende_hus_hvit.svg"
+          alt="OBOS logo"
+        />
+        <figcaption className="description">
+          OBOS sin logo har hvit tekst, og bildet må derfor ha en mørk bakgrunn.
+          Slik at man kan se hva det står.
+        </figcaption>
+      </figure>
     </div>
   );
 };
