431d514579bac5c4ccdc4a0cbcdb046c98fb3338
Oscar Carlström <oscar.carlstrom@gmail.com>
2024-10-02 14:35:09 +0200

    Standardize focus

--- packages/react/src/checkbox/Checkbox.tsx
+++ packages/react/src/checkbox/Checkbox.tsx
@@ -29,7 +29,7 @@ function CheckmarkBox() {
         // selected
         'group-data-[selected]:!border-green group-data-[selected]:!bg-green',
         // focus
-        'group-data-[focus-visible]:ring-2 group-data-[focus-visible]:ring-black group-data-[focus-visible]:ring-offset-[9px]',
+        'group-data-[focus-visible]:outline-offset-focus-black',
         // hovered
         'group-data-[hovered]:border-green group-data-[hovered]:group-data-[invalid]:border-red group-data-[hovered]:bg-green-lightest group-data-[hovered]:group-data-[invalid]:bg-red-light',
         // invalid - The border is 1 px thicker when invalid. We don't actually want to change the border width, as that causes the element's size to change
