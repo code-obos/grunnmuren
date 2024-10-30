f38ffc491768a9360624daeb8d03572f5b171438
Oscar Carlström <oscar.carlstrom@gmail.com>
2024-10-30 17:51:56 +0100

    WIP Card/Cards stories

---
+++ packages/react/src/cards/Card.stories.tsx
@@ -0,0 +1,237 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Card, Overlay } from './Cards';
+import { Heading, Media } from '../content';
+import { Button } from '../button';
+import { ArrowRight, LinkExternal } from '@obosbbl/grunnmuren-icons-react';
+
+const meta: Meta<typeof Card> = {
+  title: 'Card',
+  component: Card,
+  render: (props) => (
+    <Card {...props}>
+      <Media>
+        <img src="https://res.cloudinary.com/obosit-prd-ch-clry/image/upload/obos-logo-socialmeta.jpg" />
+      </Media>
+      <Heading level={3}>Card 1</Heading>
+      <p>Dette er en beskrivelse</p>
+    </Card>
+  ),
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Card>;
+
+const defaultProps = {
+  className: 'w-64',
+} as const;
+
+export const LinkCard: Story = {
+  args: {
+    href: '/#',
+    ...defaultProps,
+  },
+};
+
+export const LinkCardWithoutImage = () => (
+  <Card href="/#" border="black">
+    <Heading level={3}>Kort uten bilde</Heading>
+    <p>Dette er en beskrivelse</p>
+    <ArrowRight className="transition-transform group-hover/card:motion-safe:translate-x-1" />
+  </Card>
+);
+
+export const LinkCardWithoutImageAndCTA = () => (
+  <Card border="black">
+    <Heading level={3}>Kort uten bilde</Heading>
+    <p>Dette er en beskrivelse</p>
+    <Button variant="tertiary" href="#cta" className="group">
+      Les mer
+      <ArrowRight className="transition-transform group-hover:motion-safe:translate-x-1" />
+    </Button>
+  </Card>
+);
+
+export const LinkCardWithImportantOverlay = () => (
+  <Card border="black">
+    <Overlay color="blue-dark">Viktig - må komme først</Overlay>
+    <Media>
+      <img src="https://res.cloudinary.com/obosit-prd-ch-clry/image/upload/obos-logo-socialmeta.jpg" />
+    </Media>
+    <Heading level={3}>Overskrift</Heading>
+    <p>
+      Her er informasjonen i ekstra viktig, og derfor plassert før allt annt
+    </p>
+    <Button variant="tertiary" href="#cta" className="group">
+      Les mer
+      <ArrowRight className="transition-transform group-hover:motion-safe:translate-x-1" />
+    </Button>
+  </Card>
+);
+
+export const LinkCardWithOverlay = () => (
+  <Card border="black">
+    <Media>
+      <img src="https://res.cloudinary.com/obosit-prd-ch-clry/image/upload/obos-logo-socialmeta.jpg" />
+    </Media>
+    <Heading level={3}>Overskrift</Heading>
+    <Overlay color="mint">Info</Overlay>
+    <p>
+      Her er overlay plassert rett under overskriften, siden den ikke er kritisk
+    </p>
+    <Button variant="tertiary" href="#cta" className="group">
+      Les mer
+      <ArrowRight className="transition-transform group-hover:motion-safe:translate-x-1" />
+    </Button>
+  </Card>
+);
+
+export const BorderedCard: Story = {
+  args: {
+    href: '/#',
+    border: 'black',
+    ...defaultProps,
+  },
+};
+
+export const BorderedCardWithCtaInside = () => (
+  <Card border="dark-blue">
+    <Media>
+      <img src="https://res.cloudinary.com/obosit-prd-ch-clry/image/upload/obos-logo-socialmeta.jpg" />
+    </Media>
+    <Heading level={3}>Card 1</Heading>
+    <p>Dette er en beskrivelse</p>
+    <Button variant="tertiary" href="#cta" className="group">
+      Les mer
+      <ArrowRight className="transition-transform group-hover:motion-safe:translate-x-1" />
+    </Button>
+  </Card>
+);
+
+export const LinkCardWithPrimaryCTA = () => (
+  <Card border="dark-blue" href="#card">
+    <Media>
+      <img src="https://res.cloudinary.com/obosit-prd-ch-clry/image/upload/obos-logo-socialmeta.jpg" />
+    </Media>
+    <Heading level={3}>Card 1</Heading>
+    <p>Dette er en beskrivelse</p>
+    <Button variant="primary" href="#cta" className="group">
+      Meld forkjøp
+      <ArrowRight className="transition-transform group-hover:motion-safe:translate-x-1" />
+    </Button>
+    <Button variant="secondary" href="#secondary" className="group/secondary">
+      Bli medlem
+      <ArrowRight className="transition-transform group-hover/secondary:motion-safe:translate-x-1" />
+    </Button>
+  </Card>
+);
+
+export const LinkCardWithTertiaryCTA = () => (
+  <Card border="dark-blue" href="#card">
+    <Media>
+      <img src="https://res.cloudinary.com/obosit-prd-ch-clry/image/upload/obos-logo-socialmeta.jpg" />
+    </Media>
+    <Heading level={3}>Card 1</Heading>
+    <p>Dette er en beskrivelse</p>
+    <Button variant="tertiary" href="#cta" className="group">
+      Åpne finnannonsen
+      <LinkExternal />
+    </Button>
+  </Card>
+);
+
+export const CardWithCtaMultipleCTAInside = () => (
+  <Card border="dark-blue">
+    <Media>
+      <img src="https://res.cloudinary.com/obosit-prd-ch-clry/image/upload/obos-logo-socialmeta.jpg" />
+    </Media>
+    <Heading level={3}>Card 1</Heading>
+    <p>Dette er en beskrivelse</p>
+    <Button variant="secondary" href="#secondary" className="group/secondary">
+      Bli medlem
+      <ArrowRight className="transition-transform group-hover/secondary:motion-safe:translate-x-1" />
+    </Button>
+    <Button variant="tertiary" href="#tertiary" className="group/tertiary">
+      Min side
+      <ArrowRight className="transition-transform group-hover/tertiary:motion-safe:translate-x-1" />
+    </Button>
+  </Card>
+);
+
+export const LinkCardWithCtaMultipleCTAInside = () => (
+  <Card border="dark-blue" href="#card">
+    <Media>
+      <img src="https://res.cloudinary.com/obosit-prd-ch-clry/image/upload/obos-logo-socialmeta.jpg" />
+    </Media>
+    <Heading level={3}>Card 1</Heading>
+    <p>Dette er en beskrivelse</p>
+    <Button variant="secondary" href="#secondary" className="group/secondary">
+      Bli medlem
+      <ArrowRight className="transition-transform group-hover/secondary:motion-safe:translate-x-1" />
+    </Button>
+    <Button variant="tertiary" href="#tertiary" className="group/tertiary">
+      Min side
+      <ArrowRight className="transition-transform group-hover/tertiary:motion-safe:translate-x-1" />
+    </Button>
+  </Card>
+);
+
+export const InfoCard = () => (
+  <Card className="bg-sky-lightest">
+    <Heading level={3}>Informasjonskort</Heading>
+    <p>
+      Dette kortet skal ikke være klikkbart, men kan ha klikkbare elementer inne
+      i seg
+    </p>
+    <Button variant="tertiary" href="#" className="group">
+      Les mer
+      <ArrowRight className="transition-transform group-hover:motion-safe:translate-x-1" />
+    </Button>
+  </Card>
+);
+
+export const InfoCardWithMultipleCTA = () => (
+  <Card className="bg-sky-lightest">
+    <Heading level={3}>Informasjonskort</Heading>
+    <p>
+      Dette kortet skal ikke være klikkbart, men kan ha klikkbare elementer inne
+      i seg
+    </p>
+    <Button variant="secondary" href="#secondary" className="group/secondary">
+      Bli medlem
+      <ArrowRight className="transition-transform group-hover/secondary:motion-safe:translate-x-1" />
+    </Button>
+    <Button variant="tertiary" href="#tertiary" className="group/tertiary">
+      Min side
+      <ArrowRight className="transition-transform group-hover/tertiary:motion-safe:translate-x-1" />
+    </Button>
+  </Card>
+);
+
+export const InfoCardDark = () => (
+  <Card className="bg-blue-dark text-white">
+    <Heading level={3}>Informasjonskort</Heading>
+    <p>
+      Dette kortet skal ikke være klikkbart, men kan ha klikkbare elementer inne
+      i seg
+    </p>
+    <Button variant="tertiary" href="#" className="group" color="white">
+      Les mer
+      <ArrowRight className="transition-transform group-hover:motion-safe:translate-x-1" />
+    </Button>
+  </Card>
+);
+
+export const InfoCardWithInlineContent = () => (
+  <Card className="bg-blue-dark text-white">
+    <Heading level={3}>Kort med flere klikkbare elementer</Heading>
+    <p>
+      Dette kortet skal ikke være klikkbart, men kan ha klikkbare elementer inne
+      i seg <a href="#forkjop">forkjøp</a>.
+    </p>
+    <Button variant="tertiary" href="#" className="group" color="white">
+      Les mer
+      <ArrowRight className="transition-transform group-hover:motion-safe:translate-x-1" />
+    </Button>
+  </Card>
+);
---
+++ packages/react/src/cards/Cards.stories.tsx
@@ -0,0 +1,96 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Cards, Card } from './Cards';
+import { Heading, Content, Media } from '../content';
+import { ArrowRight } from '@obosbbl/grunnmuren-icons-react';
+
+const meta: Meta<typeof Cards> = {
+  title: 'Cards',
+  component: Cards,
+  render: (props) => (
+    <Cards {...props}>
+      <Card href="card-1">
+        <Media>
+          <img src="https://res.cloudinary.com/obosit-prd-ch-clry/image/upload/obos-logo-socialmeta.jpg" />
+        </Media>
+        <Heading level={3}>Kort 1</Heading>
+        <Content>
+          <p>Dette er en beskrivelse for kort 1</p>
+        </Content>
+      </Card>
+      <Card href="card-2">
+        <Media>
+          <img src="https://res.cloudinary.com/obosit-prd-ch-clry/image/upload/obos-logo-socialmeta.jpg" />
+        </Media>
+        <Heading level={3}>Kort 2</Heading>
+        <Content>
+          <p>Dette er en beskrivelse for kort 2</p>
+        </Content>
+      </Card>
+      <Card href="card-3">
+        <Media>
+          <img src="https://res.cloudinary.com/obosit-prd-ch-clry/image/upload/obos-logo-socialmeta.jpg" />
+        </Media>
+        <Heading level={3}>Kort 3</Heading>
+        <Content>
+          <p>Dette er en beskrivelse for kort 3</p>
+        </Content>
+      </Card>
+      <Card href="card-4">
+        <Media>
+          <img src="https://res.cloudinary.com/obosit-prd-ch-clry/image/upload/obos-logo-socialmeta.jpg" />
+        </Media>
+        <Heading level={3}>Kort 4</Heading>
+        <Content>
+          <p>Dette er en beskrivelse for kort 4</p>
+        </Content>
+      </Card>
+    </Cards>
+  ),
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Cards>;
+
+const defaultProps = {
+  className: 'grid grid-cols-2 gap-6',
+} as const;
+
+export const CardsWithImage: Story = {
+  args: {
+    ...defaultProps,
+  },
+};
+
+export const CardsWithoutImage = () => (
+  <Cards {...defaultProps}>
+    <Card href="card-1" border="black">
+      <Heading level={3}>Kort 1</Heading>
+      <Content>
+        <p>Dette er en beskrivelse for kort 1</p>
+      </Content>
+      <ArrowRight className="transition-transform group-hover/card:motion-safe:translate-x-1" />
+    </Card>
+    <Card href="card-2" border="black">
+      <Heading level={3}>Kort 2</Heading>
+      <Content>
+        <p>Dette er en beskrivelse for kort 2</p>
+      </Content>
+      <ArrowRight className="transition-transform group-hover/card:motion-safe:translate-x-1" />
+    </Card>
+    <Card href="card-3" border="black">
+      <Heading level={3}>Kort 3</Heading>
+      <Content>
+        <p>Dette er en beskrivelse for kort 3</p>
+      </Content>
+      <ArrowRight className="transition-transform group-hover/card:motion-safe:translate-x-1" />
+    </Card>
+    <Card href="card-4" border="black">
+      <Heading level={3}>Kort 4</Heading>
+      <Content>
+        <p>Dette er en beskrivelse for kort 4</p>
+      </Content>
+      <ArrowRight className="transition-transform group-hover/card:motion-safe:translate-x-1" />
+    </Card>
+  </Cards>
+);
