09a0f3ab3e8ca01794f128880697ed29e651e7c4
Oscar Carlström <oscar.carlstrom@gmail.com>
2024-11-13 16:16:47 +0100

    Replace href prop with CardLink component

--- packages/react/src/card/Card.tsx
+++ packages/react/src/card/Card.tsx
@@ -1,12 +1,10 @@
 import { cva, cx, VariantProps } from 'cva';
-import { Link, LinkProps, Provider } from 'react-aria-components';
-import { HeadingContext } from '../content';
+import { Link, LinkProps } from 'react-aria-components';
 
 type CardProps = VariantProps<typeof cardVariants> & {
   children?: React.ReactNode;
   className?: string;
   /** Makes the entire card clickable by wrapping the content of the <Heading> in a <Link> */
-  href?: LinkProps['href'];
 };
 
 const cardVariants = cva({
@@ -15,6 +13,13 @@ const cardVariants = cva({
     'rounded-2xl border p-3',
     'grid auto-rows-max gap-y-4',
     'relative', // Needed for positiong of the clickable pseudo-element (and can also be used for other absolute positioned elements the consumer might add)
+
+    // **** Heading ****
+    '[&_[data-slot="heading"]]:inline',
+    '[&:not(:has([data-slot="card-link"]))_[data-slot="heading"]]:heading-s',
+    '[&:not(:has([data-slot="card-link"]))_[data-slot="heading"]]:w-fit',
+    '[&:not(:has([data-slot="card-link"]))_[data-slot="heading"]]:text-pretty',
+
     // **** Content ****
     '[&_[data-slot="content"]]:grid [&_[data-slot="content"]]:auto-rows-max [&_[data-slot="content"]]:gap-y-4',
 
@@ -26,7 +31,37 @@ const cardVariants = cva({
     // Sets the aspect ratio of the media content (width: 100% is necessary to make aspect ratio work in FF)
     '[&_[data-slot="media"]>*]:aspect-[3/2] [&_[data-slot="media"]>*]:w-full [&_[data-slot="media"]_img]:object-cover',
     // Prepare zoom animation for hover effects. The hover effect can also be enabled by classes on the parent component, so it is always prepared here.
-    '[&_[data-slot="media"]>*]:transition-transform [&_[data-slot="media"]>*]:duration-300 [&_[data-slot="media"]>*]:ease-in-out',
+    '[&_[data-slot="media"]>*]:duration-300 [&_[data-slot="media"]>*]:ease-in-out [&_[data-slot="media"]>*]:motion-safe:transition-transform',
+
+    // **** Card link ****
+    // Enables the zoom hover effect on media (note that we can't use group-hover/card here, because there might be other clickable elements in the card aside from the heading)
+    '[&:has([data-slot="card-link"]:hover)_[data-slot="media"]>*]:scale-110',
+    // **** Card link in Heading ****
+    // **** Hover: custom underline ****
+    // The border radius needs to be adjusted to align perfectly with the card border
+    '[&_[data-slot="heading"]_[data-slot="card-link"]]:no-underline',
+    '[&_[data-slot="heading"]_[data-slot="card-link"]]:after:absolute',
+    '[&_[data-slot="heading"]_[data-slot="card-link"]]:after:inset-[calc(theme(borderWidth.DEFAULT)*-1)]',
+    '[&_[data-slot="heading"]_[data-slot="card-link"]]:after:rounded-[calc(theme(borderRadius.2xl)-theme(borderWidth.DEFAULT))]',
+    // Border (bottom/top) is set to transparent to make sure the bottom underline is not visible when the card is hovered
+    // Border top is set to even out the border bottom used for the underline
+    '[&_[data-slot="heading"]_[data-slot="card-link"]]:border-y-2',
+    '[&_[data-slot="heading"]_[data-slot="card-link"]]:border-y-transparent',
+    '[&_[data-slot="heading"]_[data-slot="card-link"]]:transition-colors',
+    '[&_[data-slot="heading"]_[data-slot="card-link"]]:hover:border-b-current',
+    // **** Focus ****
+    '[&_[data-slot="heading"]_[data-slot="card-link"]:focus-visible]:after:outline-focus',
+    '[&_[data-slot="heading"]_[data-slot="card-link"]:focus-visible]:outline-none',
+    '[&_[data-slot="heading"]_[data-slot="card-link"]:focus-visible]:after:outline-offset-2',
+    // Mimic heading styles for the card link if placed in the heading slot
+    '[&_[data-slot="heading"]_[data-slot="card-link"]]:heading-s [&_[data-slot="heading"]_[data-slot="card-link"]]:text-pretty',
+
+    // **** Fail-safe for interactive elements ****
+    // Make interactive elements clickable by themselves, while the rest of the card is clickable as a whole
+    // The card is then made clickable by a pseudo-element on the heading that covers the entire card
+    '[&_a:not([data-slot="card-link"])]:relative [&_button]:relative [&_input]:relative',
+    // Place other interactive on top of the pseudo-element that makes the entire card clickable by setting a higher z-index than the pseudo-element
+    '[&_a:not([data-slot="card-link"])]:z-[2] [&_button]:z-[2] [&_input]:z-[2]',
   ],
   variants: {
     border: {
@@ -39,20 +74,6 @@ const cardVariants = cva({
         '[&_[data-slot="media"]]:rounded-b-2xl',
       ],
     },
-    hasHref: {
-      false: '',
-      true: [
-        // Enables the zoom hover effect on media (note that we can't use group-hover/card here, because there might be other clickable elements in the card aside from the heading)
-        '[&:has([data-slot="card-heading-link"]:hover)_[data-slot="media"]>*]:scale-110',
-
-        // **** Fail-safe for interactive elements ****
-        // Make interactive elements clickable by themselves, while the rest of the card is clickable as a whole
-        // The card is then made clickable by a pseudo-element on the heading that covers the entire card
-        '[&_a:not([data-slot="card-heading-link"])]:relative [&_button]:relative [&_input]:relative',
-        // Place other interactive on top of the pseudo-element that makes the entire card clickable by setting a higher z-index than the pseudo-element
-        '[&_a:not([data-slot="card-heading-link"])]:z-[2] [&_button]:z-[2] [&_input]:z-[2]',
-      ],
-    },
   },
 });
 
@@ -60,59 +81,27 @@ const Card = ({
   children,
   className: _className,
   border,
-  href,
   ...restProps
 }: CardProps) => {
   const className = cardVariants({
     className: _className,
     border,
-    hasHref: !!href,
   });
   return (
     <div className={className} {...restProps}>
-      <Provider
-        values={[
-          [
-            HeadingContext,
-            {
-              className: cx(
-                'inline',
-                // Styles for the heading when content is not wrapped in a link
-                !href && 'heading-s w-fit text-pretty',
-              ),
-              _innerWrapper: (children) =>
-                href ? (
-                  <Link
-                    href={href}
-                    // Uses a pseudo-element with absolute position to make the entire card focusable and clickable
-                    className={cx(
-                      'cursor-pointer',
-                      // **** Hover: custom underline ****
-                      // The border radius needs to be adjusted to align perfectly with the card border
-                      'no-underline after:absolute after:inset-[calc(theme(borderWidth.DEFAULT)*-1)] after:rounded-[calc(theme(borderRadius.2xl)-theme(borderWidth.DEFAULT))]',
-                      // Border (bottom/top) is set to transparent to make sure the bottom underline is not visible when the card is hovered
-                      // Border top is set to even out the border bottom used for the underline
-                      'border-y-2 border-y-transparent transition-colors hover:border-b-current',
-                      // Match the heading styles (especially important when the content spans mulitple lines)
-                      'heading-s text-pretty',
-                      // **** Focus ****
-                      'focus-visible:after:outline-focus focus-visible:outline-none focus-visible:after:outline-offset-2',
-                    )}
-                    data-slot="card-heading-link"
-                  >
-                    {children}
-                  </Link>
-                ) : (
-                  children
-                ),
-            },
-          ],
-        ]}
-      >
-        {children}
-      </Provider>
+      {children}
     </div>
   );
 };
 
-export { Card, type CardProps };
+type CardLinkProps = LinkProps;
+
+const CardLink = ({ className, ...restProps }: CardLinkProps) => (
+  <Link
+    data-slot="card-link"
+    {...restProps}
+    className={cx(className, 'cursor-pointer')}
+  />
+);
+
+export { Card, type CardProps, type CardLinkProps, CardLink };
