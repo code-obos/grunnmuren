00e0eea8c30794dd67f33a2c3d29992d2925ae38
Oscar Carlström <oscar.carlstrom@gmail.com>
2024-11-19 12:47:28 +0100

    Date formatter (#976)
    * Add DateFormatter
    
    * Add examples
    
    * Move declartion
    
    * Update lockfile
    
    * Add changeset
    
    * Change render prop to children
    
    * Add version range to react-aria
    
    * Fix lockfile
    
    * Remove fragment

---
+++ packages/react/src/dateformatter/DateFormatter.stories.tsx
@@ -0,0 +1,126 @@
+import type { Meta } from '@storybook/react';
+import { DateFormatter } from './DateFormatter';
+
+const meta: Meta = {
+  title: 'DateFormatter',
+};
+
+export const Default = () => <DateFormatter value={new Date()} />;
+
+export const LongDate = () => (
+  <DateFormatter
+    value={new Date()}
+    options={{
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    }}
+  />
+);
+
+export const FullDateTime = () => (
+  <DateFormatter
+    value={new Date()}
+    options={{
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+    }}
+  />
+);
+
+export const WithCustomTimeZone = () => (
+  <DateFormatter
+    value={new Date()}
+    options={{
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+      timeZone: 'EST',
+    }}
+  />
+);
+
+export const Year = () => (
+  <DateFormatter
+    value={new Date()}
+    options={{
+      year: 'numeric',
+    }}
+  />
+);
+
+export const ShortYear = () => (
+  <DateFormatter
+    value={new Date()}
+    options={{
+      year: '2-digit',
+    }}
+  />
+);
+
+export const Month = () => (
+  <DateFormatter
+    value={new Date()}
+    options={{
+      month: 'numeric',
+    }}
+  />
+);
+
+export const LongMonth = () => (
+  <DateFormatter
+    value={new Date()}
+    options={{
+      month: 'long',
+    }}
+  />
+);
+
+export const ShortMonth = () => (
+  <DateFormatter
+    value={new Date()}
+    options={{
+      month: 'short',
+    }}
+  />
+);
+
+export const CapitalizedMonth = () => (
+  <DateFormatter
+    value={new Date()}
+    options={{
+      month: 'long',
+    }}
+  >
+    {(formattedDate: string) =>
+      formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1)
+    }
+  </DateFormatter>
+);
+
+export const Day = () => (
+  <DateFormatter
+    value={new Date()}
+    options={{
+      day: 'numeric',
+    }}
+  />
+);
+
+export const ShortDay = () => (
+  <DateFormatter
+    value={new Date()}
+    options={{
+      day: '2-digit',
+    }}
+  />
+);
+
+export default meta;
