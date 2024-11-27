1d90391ef74c1d5ded9ca4ed67ec2b4b20fa9fdf
Alexander Bjerkan <alexander.bjerkan@obos.no>
2024-11-27 09:58:14 +0100

    docs: add icon page (#995)

---
+++ apps/docs/app/routes/ikoner.tsx
@@ -0,0 +1,26 @@
+import * as icons from '@obosbbl/grunnmuren-icons-react';
+import { Card } from '@obosbbl/grunnmuren-react';
+import { createFileRoute } from '@tanstack/react-router';
+
+export const Route = createFileRoute('/ikoner')({
+  component: IconsGrid,
+});
+
+function IconsGrid() {
+  return (
+    <div className="grid grid-cols-[repeat(auto-fill,_150px)] content-center justify-center gap-6">
+      {Object.entries(icons).map(([iconName, Icon]) => (
+        <IconCard key={iconName} iconName={iconName} Icon={Icon} />
+      ))}
+    </div>
+  );
+}
+
+function IconCard({ iconName, Icon }) {
+  return (
+    <Card className="bg-gray-lightest" key={iconName}>
+      <Icon className="mx-auto" />
+      <span className="block text-center text-sm">{iconName}</span>
+    </Card>
+  );
+}
