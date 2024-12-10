edd4b6480eb27372c3cc1117ddccfd80d1093619
Oscar Carlström <oscar.carlstrom@gmail.com>
2024-12-10 18:58:41 +0100

    Add code examples

--- apps/docs/app/routes/komponenter/badge.tsx
+++ apps/docs/app/routes/komponenter/badge.tsx
@@ -1,10 +1,71 @@
 import { PropsTable } from '@/ui/props-table';
 import { createFileRoute } from '@tanstack/react-router';
 import { BadgeDoc } from '../../../docgen';
+import { ComponentPreview } from '@/ui/component-preview';
+import { Badge } from '@obosbbl/grunnmuren-react';
+import { PaintRoller } from '@obosbbl/grunnmuren-icons-react';
+import { cx } from 'cva';
 
 export const Route = createFileRoute('/komponenter/badge')({
   component: Page,
 });
+
+const colors = [
+  'mint',
+  'sky',
+  'blue-dark',
+  'green-dark',
+  'gray-dark',
+  'white',
+] as const;
+
+const examples = [
+  {
+    title: 'Badge',
+    code: (
+      <>
+        {colors.map((color) => (
+          <Badge color={color}>{color}</Badge>
+        ))}
+      </>
+    ),
+  },
+  ...colors.map((color) => ({
+    title: color.charAt(0).toUpperCase() + color.slice(1),
+    code: (
+      <div className={cx('space-x-4 p-2', color === 'white' && 'bg-gray')}>
+        <Badge color={color} size="small">
+          small
+        </Badge>
+        <Badge color={color} size="medium">
+          medium
+        </Badge>
+        <Badge color={color} size="large">
+          large
+        </Badge>
+      </div>
+    ),
+  })),
+  ...colors.map((color) => ({
+    title: `${color.charAt(0).toUpperCase() + color.slice(1)} med ikon`,
+    code: (
+      <div className={cx('space-x-4 p-2', color === 'white' && 'bg-gray')}>
+        <Badge color={color} size="small">
+          <PaintRoller />
+          small
+        </Badge>
+        <Badge color={color} size="medium">
+          <PaintRoller />
+          medium
+        </Badge>
+        <Badge color={color} size="large">
+          <PaintRoller />
+          large
+        </Badge>
+      </div>
+    ),
+  })),
+];
 
 function Page() {
   return (
@@ -13,6 +74,16 @@ function Page() {
       <div className="prose">
         <p>{BadgeDoc.description}</p>
       </div>
+
+      {examples.map(({ title, code }) => (
+        <ComponentPreview
+          scope={{ Badge, PaintRoller }}
+          key={title}
+          title={title}
+          code={code}
+        />
+      ))}
+
       <PropsTable props={BadgeDoc.props} />
     </>
   );
