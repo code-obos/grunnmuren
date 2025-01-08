585e6daa78eff1804cf048a8fee5e575a1c897d3
Oscar Carlström <oscar.carlstrom@gmail.com>
2025-01-08 11:57:06 +0100

    Fix color definitions (#1064)
    * Fix color definitions
    
    * Convert hex to rbg values
    
    * Fixup param reassign
    
    * Fix syntax error

--- packages/tailwind/tailwind-base.cjs
+++ packages/tailwind/tailwind-base.cjs
@@ -179,39 +179,39 @@ module.exports = (options = {}) => {
             '--gm-container-width': '92rem',
             '--gm-container-gutter-width': '1rem',
 
-            '--gm-color-black': '#333',
-            '--gm-color-white': '#fff',
+            '--gm-color-black': hexToRgb('#333'),
+            '--gm-color-white': hexToRgb('#fff'),
 
-            '--gm-color-gray': '#818181',
-            '--gm-color-gray-dark': '#595959',
-            '--gm-color-gray-light': '#e6e6e6',
-            '--gm-color-gray-lightest': '#f1f1f1',
+            '--gm-color-gray': hexToRgb('#818181'),
+            '--gm-color-gray-dark': hexToRgb('#595959'),
+            '--gm-color-gray-light': hexToRgb('#e6e6e6'),
+            '--gm-color-gray-lightest': hexToRgb('#f1f1f1'),
 
-            '--gm-color-sky': '#bedfec',
-            '--gm-color-sky-light': '#deeff5',
-            '--gm-color-sky-lightest': '#ebf5f9',
+            '--gm-color-sky': hexToRgb('#bedfec'),
+            '--gm-color-sky-light': hexToRgb('#deeff5'),
+            '--gm-color-sky-lightest': hexToRgb('#ebf5f9'),
 
-            '--gm-color-mint': '#cdece2',
-            '--gm-color-mint-light': '#e6f5f0',
-            '--gm-color-mint-lightest': '#f0f9f6',
+            '--gm-color-mint': hexToRgb('#cdece2'),
+            '--gm-color-mint-light': hexToRgb('#e6f5f0'),
+            '--gm-color-mint-lightest': hexToRgb('#f0f9f6'),
 
-            '--gm-color-blue': '#0047ba',
-            '--gm-color-blue-light': '#bedfec',
-            '--gm-color-blue-lightest': '#deeff5',
-            '--gm-color-blue-dark': '#002169',
+            '--gm-color-blue': hexToRgb('#0047ba'),
+            '--gm-color-blue-light': hexToRgb('#bedfec'),
+            '--gm-color-blue-lightest': hexToRgb('#deeff5'),
+            '--gm-color-blue-dark': hexToRgb('#002169'),
 
-            '--gm-color-green': '#008761',
-            '--gm-color-green-dark': '#00524c',
-            '--gm-color-green-light': '#cdece2',
-            '--gm-color-green-lightest': '#e6f5f0',
+            '--gm-color-green': hexToRgb('#008761'),
+            '--gm-color-green-dark': hexToRgb('#00524c'),
+            '--gm-color-green-light': hexToRgb('#cdece2'),
+            '--gm-color-green-lightest': hexToRgb('#e6f5f0'),
 
-            '--gm-color-red': '#c0385d',
-            '--gm-color-red-light': '#faedef',
+            '--gm-color-red': hexToRgb('#c0385d'),
+            '--gm-color-red-light': hexToRgb('#faedef'),
 
-            '--gm-color-orange': '#e8a74a',
-            '--gm-color-orange-light': '#f8e5c9',
+            '--gm-color-orange': hexToRgb('#e8a74a'),
+            '--gm-color-orange-light': hexToRgb('#f8e5c9'),
 
-            '--gm-color-yellow': '#fff5d2',
+            '--gm-color-yellow': hexToRgb('#fff5d2'),
           },
         });
 
@@ -386,52 +386,52 @@ module.exports = (options = {}) => {
         inherit: 'inherit',
         current: 'currentColor',
         transparent: 'transparent',
-        black: 'var(--gm-color-black)',
-        white: 'var(--gm-color-white)',
+        black: 'rgb(var(--gm-color-black))',
+        white: 'rgb(var(--gm-color-white))',
         gray: {
-          DEFAULT: 'var(--gm-color-gray)',
-          dark: 'var(--gm-color-gray-dark)',
-          light: 'var(--gm-color-gray-light)',
-          lightest: 'var(--gm-color-gray-lightest)',
+          DEFAULT: 'rgb(var(--gm-color-gray))',
+          dark: 'rgb(var(--gm-color-gray-dark))',
+          light: 'rgb(var(--gm-color-gray-light))',
+          lightest: 'rgb(var(--gm-color-gray-lightest))',
         },
         sky: {
-          DEFAULT: 'var(--gm-color-sky)',
-          light: 'var(--gm-color-sky-light)',
-          lightest: 'var(--gm-color-sky-lightest)',
+          DEFAULT: 'rgb(var(--gm-color-sky))',
+          light: 'rgb(var(--gm-color-sky-light))',
+          lightest: 'rgb(var(--gm-color-sky-lightest))',
         },
         mint: {
-          DEFAULT: 'var(--gm-color-mint)',
-          light: 'var(--gm-color-mint-light)',
-          lightest: 'var(--gm-color-mint-lightest)',
+          DEFAULT: 'rgb(var(--gm-color-mint))',
+          light: 'rgb(var(--gm-color-mint-light))',
+          lightest: 'rgb(var(--gm-color-mint-lightest))',
         },
         blue: {
           // OBOS Blue/Primary brand
-          DEFAULT: 'var(--gm-color-blue)',
+          DEFAULT: 'rgb(var(--gm-color-blue))',
           // OBOS Ocean
-          dark: 'var(--gm-color-blue-dark)',
-          light: 'var(--gm-color-blue-light)',
-          lightest: 'var(--gm-color-blue-lightest)',
+          dark: 'rgb(var(--gm-color-blue-dark))',
+          light: 'rgb(var(--gm-color-blue-light))',
+          lightest: 'rgb(var(--gm-color-blue-lightest))',
         },
         green: {
           // OBOS Green/Primary brand
-          DEFAULT: 'var(--gm-color-green)',
+          DEFAULT: 'rgb(var(--gm-color-green))',
           // OBOS Forest
-          dark: 'var(--gm-color-green-dark)',
-          light: 'var(--gm-color-green-light)',
-          lightest: 'var(--gm-color-green-lightest)',
+          dark: 'rgb(var(--gm-color-green-dark))',
+          light: 'rgb(var(--gm-color-green-light))',
+          lightest: 'rgb(var(--gm-color-green-lightest))',
         },
         red: {
-          DEFAULT: 'var(--gm-color-red)',
+          DEFAULT: 'rgb(var(--gm-color-red))',
           // error red
-          light: 'var(--gm-color-red-light)',
+          light: 'rgb(var(--gm-color-red-light))',
         },
         orange: {
-          DEFAULT: 'var(--gm-color-orange)',
-          light: 'var(--gm-color-orange-light)',
+          DEFAULT: 'rgb(var(--gm-color-orange))',
+          light: 'rgb(var(--gm-color-orange-light))',
         },
         yellow: {
           // open house
-          DEFAULT: 'var(--gm-color-yellow)',
+          DEFAULT: 'rgb(var(--gm-color-yellow))',
         },
       },
       fontFamily: {
@@ -571,6 +571,32 @@ module.exports = (options = {}) => {
     },
   };
 };
+
+/**
+ * A function that takes in a hex color as a string and returns a string with the color's rgb values.
+ * @param {string} hex
+ * @returns {string} The rgb values of the color.
+ */
+function hexToRgb(hex) {
+  // Remove the hash (#) at the start if it's there
+  let hexValue = hex.replace(/^#/, '');
+
+  // If the hex value is shorthand (3 characters), expand it to 6 characters
+  if (hexValue.length === 3) {
+    hexValue = hexValue
+      .split('')
+      .map((char) => char + char)
+      .join('');
+  }
+
+  // Parse the r, g, and b values from the hex string
+  const r = Number.parseInt(hexValue.substring(0, 2), 16);
+  const g = Number.parseInt(hexValue.substring(2, 4), 16);
+  const b = Number.parseInt(hexValue.substring(4, 6), 16);
+
+  // Return the RGB values as a whitespace-separated string
+  return `${r} ${g} ${b}`;
+}
 
 // These custom components are only used for v1 compat
 const button = plugin(({ addComponents, theme }) => {
