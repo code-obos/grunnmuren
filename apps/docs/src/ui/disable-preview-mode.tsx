b932e746334d37574a155f534c1d76e6ae48bbbc
Alexander Bjerkan <alexander.bjerkan@obos.no>
2025-01-12 08:11:34 +0100

    partially working

---
+++ apps/docs/src/ui/disable-preview-mode.tsx
@@ -0,0 +1,10 @@
+export function DisablePreviewMode() {
+  return (
+    <a
+      href="/api/preview-mode/disable"
+      className="block bg-blue py-2 text-center text-white"
+    >
+      Disable preview mode
+    </a>
+  );
+}
