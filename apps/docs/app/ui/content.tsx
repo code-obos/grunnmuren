ebbcddc5a3a0f6708ea545ac73537402800cc167
Oscar Carlström <oscar.carlstrom@gmail.com>
2025-01-15 11:36:24 +0100

    Fix TS error

--- apps/docs/app/ui/content.tsx
+++ apps/docs/app/ui/content.tsx
@@ -9,40 +9,44 @@ export type ContentProps = {
   className?: string;
 };
 
-const components = {
-  types: {
-    'live-code-block': ({ value }) => (
-      <ComponentPreview caption={value.caption} code={value.code.code} />
-    ),
-  },
-  block: {
-    h2: ({ children, node }) => (
-      <AnchorHeading level={2} id={node._key}>
-        {children}
-      </AnchorHeading>
-    ),
-    h3: ({ children, node }) => (
-      <AnchorHeading level={3} id={node._key}>
-        {children}
-      </AnchorHeading>
-    ),
-    h4: ({ children, node }) => (
-      <AnchorHeading level={4} id={node._key}>
-        {children}
-      </AnchorHeading>
-    ),
-    h5: ({ children, node }) => (
-      <AnchorHeading level={5} id={node._key}>
-        {children}
-      </AnchorHeading>
-    ),
-  },
-};
-
 export function Content({ content, className }: ContentProps) {
   return (
     <div className={cx('prose', className)}>
-      <PortableText value={content} components={components} />
+      <PortableText
+        value={content}
+        components={{
+          types: {
+            'live-code-block': ({ value }) => (
+              <ComponentPreview
+                caption={value.caption}
+                code={value.code.code}
+              />
+            ),
+          },
+          block: {
+            h2: ({ children, value }) => (
+              <AnchorHeading level={2} id={value._key ?? ''}>
+                {children}
+              </AnchorHeading>
+            ),
+            h3: ({ children, value }) => (
+              <AnchorHeading level={3} id={value._key ?? ''}>
+                {children}
+              </AnchorHeading>
+            ),
+            h4: ({ children, value }) => (
+              <AnchorHeading level={4} id={value._key ?? ''}>
+                {children}
+              </AnchorHeading>
+            ),
+            h5: ({ children, value }) => (
+              <AnchorHeading level={5} id={value._key ?? ''}>
+                {children}
+              </AnchorHeading>
+            ),
+          },
+        }}
+      />
     </div>
   );
 }
