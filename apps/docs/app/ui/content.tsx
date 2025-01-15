4ae83727c1b4f972a5d1e76c7a5a1c2790e2ace6
Oscar Carlström <oscar.carlstrom@gmail.com>
2025-01-15 13:55:14 +0100

    Organize imports

--- apps/docs/app/ui/content.tsx
+++ apps/docs/app/ui/content.tsx
@@ -1,11 +1,11 @@
 import { PortableText } from '@portabletext/react';
 import { cx } from 'cva';
+import { themes } from 'prism-react-renderer';
+import { LiveEditor, LiveProvider } from 'react-live';
 import type { Content as IContent } from 'sanity.types';
 import { AnchorHeading } from './anchor-heading';
-import { ComponentPreview } from './component-preview';
-import { LiveEditor, LiveProvider } from 'react-live';
-import { themes } from 'prism-react-renderer';
 import { Code } from './code';
+import { ComponentPreview } from './component-preview';
 
 export type ContentProps = {
   content: IContent;
