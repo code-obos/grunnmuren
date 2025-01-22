4ad2d8bdf1bf60b8437efbe63cb08098e7d7d345
Oscar Carlström <oscar.carlstrom@gmail.com>
2025-01-22 08:27:52 +0100

    Split up in several stories

--- packages/react/src/disclosure/disclosure.stories.tsx
+++ packages/react/src/disclosure/disclosure.stories.tsx
@@ -1,7 +1,8 @@
+import { Menu } from '@obosbbl/grunnmuren-icons-react';
 import type { Meta, StoryObj } from '@storybook/react';
-
+import { Checkbox, CheckboxGroup } from '../checkbox';
 import { Disclosure, DisclosureButton, DisclosurePanel } from './disclosure';
-import { ChevronDown, Menu } from '@obosbbl/grunnmuren-icons-react';
+import { useId } from 'react';
 
 const meta: Meta<typeof Disclosure> = {
   title: 'Disclosure',
@@ -12,35 +13,10 @@ const meta: Meta<typeof Disclosure> = {
   },
   render: (props) => {
     return (
-      <div className="grid gap-12 p-4">
-        <Disclosure {...props}>
-          <DisclosureButton
-            aria-label="Meny"
-            className="grid place-items-center"
-            variant="dense"
-          >
-            <Menu />
-          </DisclosureButton>
-          <DisclosurePanel>
-            <p>Her finner du alle detaljer du måtte trenge.</p>
-          </DisclosurePanel>
-        </Disclosure>
-
+      <div className="p-4">
         <Disclosure {...props}>
           <DisclosureButton className="grid place-items-center">
             Les mer
-          </DisclosureButton>
-          <DisclosurePanel>
-            <p>Her finner du alle detaljer du måtte trenge.</p>
-          </DisclosurePanel>
-        </Disclosure>
-
-        <Disclosure {...props}>
-          <DisclosureButton
-            className="flex gap-6 rounded-lg border-gray-light px-2 py-3.5"
-            withChevron
-          >
-            Her kan du velge mye forskjellig{' '}
           </DisclosureButton>
           <DisclosurePanel>
             <p>Her finner du alle detaljer du måtte trenge.</p>
@@ -57,4 +33,51 @@ type Story = StoryObj<typeof Disclosure>;
 
 export const DisclosureStory: Story = {
   args: {},
+};
+
+export const Dense: Story = {
+  render: (props) => {
+    return (
+      <div className="p-4">
+        <Disclosure {...props}>
+          <DisclosureButton
+            aria-label="Meny"
+            className="grid place-items-center"
+            variant="dense"
+          >
+            <Menu />
+          </DisclosureButton>
+          <DisclosurePanel>
+            <p>Her finner du alle detaljer du måtte trenge.</p>
+          </DisclosurePanel>
+        </Disclosure>
+      </div>
+    );
+  },
+};
+
+export const WithChevron: Story = {
+  render: (props) => {
+    const id = useId();
+    return (
+      <div className="p-4">
+        <Disclosure {...props}>
+          <DisclosureButton
+            className="flex gap-6 rounded-lg border-gray-light px-2 py-3.5"
+            withChevron
+            id={id}
+          >
+            Kjøpsalternativer
+          </DisclosureButton>
+          <DisclosurePanel className="px-4">
+            <CheckboxGroup aria-labelledby={id}>
+              <Checkbox value="deleie">Deleie</Checkbox>
+              <Checkbox value="bostart">Bostart</Checkbox>
+              <Checkbox value="boligbytte">Boligbytte</Checkbox>
+            </CheckboxGroup>
+          </DisclosurePanel>
+        </Disclosure>
+      </div>
+    );
+  },
 };
