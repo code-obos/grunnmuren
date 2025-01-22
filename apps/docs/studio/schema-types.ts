4e5a0d719257f34395f23508ef911ddbce05d53b
Gabriel Wallin <gabriel.wallin@bekk.no>
2025-01-22 12:54:54 +0100

    run biome

--- apps/docs/studio/schema-types.ts
+++ apps/docs/studio/schema-types.ts
@@ -1,8 +1,8 @@
 import component from './documents/component';
 import content from './objects/content';
+import imageWithCaption from './objects/image-with-caption';
 import liveCodeBlock from './objects/live-code-block';
 import staticCodeBlock from './objects/static-code-block';
-import imageWithCaption from './objects/image-with-caption';
 
 // Export an array of all the schema types.  This is used in the Sanity Studio configuration. https://www.sanity.io/docs/schema-types
 export const schemaTypes = [
