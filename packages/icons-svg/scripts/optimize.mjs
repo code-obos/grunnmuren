eee299a7dda894b5a36468e3a3212042085a2840
Alexander Bjerkan <alexander.bjerkan@obos.no>
2025-01-31 10:44:43 +0100

    optimize icon SVGs by merging paths

--- packages/icons-svg/scripts/optimize.mjs
+++ packages/icons-svg/scripts/optimize.mjs
@@ -30,6 +30,7 @@ const config = {
     {
       name: 'sortAttrs',
     },
+    { name: 'mergePaths' }
   ],
 };
 
@@ -67,12 +68,12 @@ function printProfitInfo(inBytes, outBytes) {
   console.log(
     // biome-ignore lint/style/useTemplate: leaving this as non template literal as it is copied
     Math.round((inBytes / 1024) * 1000) / 1000 +
-      ' KiB' +
-      (profitPercents < 0 ? ' + ' : ' - ') +
-      // biome-ignore lint/style/useTemplate: leaving this as non template literal as it is copied
-      pc.green(Math.abs(Math.round(profitPercents * 10) / 10) + '%') +
-      ' = ' +
-      Math.round((outBytes / 1024) * 1000) / 1000 +
-      ' KiB',
+    ' KiB' +
+    (profitPercents < 0 ? ' + ' : ' - ') +
+    // biome-ignore lint/style/useTemplate: leaving this as non template literal as it is copied
+    pc.green(Math.abs(Math.round(profitPercents * 10) / 10) + '%') +
+    ' = ' +
+    Math.round((outBytes / 1024) * 1000) / 1000 +
+    ' KiB',
   );
 }
