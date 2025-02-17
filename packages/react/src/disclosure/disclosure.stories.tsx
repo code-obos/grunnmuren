040cc3194fdb0d79869c68fcae0831ee7acf7263
Oscar Carlström <oscar.carlstrom@gmail.com>
2025-02-17 09:48:06 +0100

    Add Disclosure (#1136)
    * Add Disclosure
    
    * Add dense/regular variants
    
    * Fix regular variant click area
    
    * Update text
    
    * Make chevron disclosure a variant
    
    * Structure imports
    
    * Fix focus styles
    
    * Split up in several stories
    
    * Add isIconOnly variant
    
    * Rename variant prop
    
    * Add comments
    
    * Fix styles
    
    * Rewrite as custom disclosure
    
    * Add animation to disclosure panel
    
    * Orginize imports
    
    * Support default role AND override
    
    * Use react-aria useDisclosure
    
    * Remove unused context
    
    * Order CSS classes
    
    * Avoid redundant aria-labeledby
    
    * Add DisclosureGroup example
    
    * Organize imports
    
    * Add changeset
    
    * Update checkbox values
    
    * Improve read more example
    
    * Use cursor: pointer
    
    * Fix group example layout
    
    * Fix hidden="until-found"
    
    * Use role none instead of presentaiton
    
    * Supress biome lint error
    
    * Fix lint errors
    
    * Format
    
    * Fix chevron styles
    
    * Fix animation on collapse
    
    * Update with main
    
    * Fix formatting
    
    * Prefix with UNSAFE_
    
    * re-export DisclosureGroup
    
    * Organize imports

---
+++ packages/react/src/disclosure/disclosure.stories.tsx
@@ -0,0 +1,182 @@
+import { Menu } from '@obosbbl/grunnmuren-icons-react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { useId, useState } from 'react';
+import { Checkbox, CheckboxGroup } from '../checkbox';
+import {
+  UNSAFE_Disclosure as Disclosure,
+  UNSAFE_DisclosureButton as DisclosureButton,
+  UNSAFE_DisclosureGroup as DisclosureGroup,
+  UNSAFE_DisclosurePanel as DisclosurePanel,
+} from './disclosure';
+
+const meta: Meta<typeof Disclosure> = {
+  title: 'Disclosure',
+  component: Disclosure,
+  parameters: {
+    // disable built in padding in story, because we provide our own
+    layout: 'fullscreen',
+  },
+  render: (props) => {
+    return (
+      <div className="p-4">
+        <Disclosure {...props}>
+          <DisclosureButton className="description" withChevron>
+            Les mer
+          </DisclosureButton>
+          <DisclosurePanel>
+            <p>Her finner du alle detaljer du måtte trenge.</p>
+          </DisclosurePanel>
+        </Disclosure>
+      </div>
+    );
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Disclosure>;
+
+export const DisclosureStory: Story = {
+  args: {},
+};
+
+export const Dense: Story = {
+  render: (props) => {
+    return (
+      <div className="p-4">
+        <Disclosure {...props}>
+          <DisclosureButton
+            aria-label="Meny"
+            className="grid place-items-center"
+            isIconOnly
+          >
+            <Menu />
+          </DisclosureButton>
+          <DisclosurePanel>
+            <p>Her finner du alle detaljer du måtte trenge.</p>
+          </DisclosurePanel>
+        </Disclosure>
+      </div>
+    );
+  },
+};
+
+export const WithCheckboxGroup: Story = {
+  render: (props) => {
+    const id = useId();
+    const [selectedOptions, setSelectedItems] = useState<string[]>([]);
+    const [isExpanded, setIsExpanded] = useState(false);
+    return (
+      <div className="p-12">
+        <Disclosure
+          {...props}
+          isExpanded={isExpanded}
+          onExpandedChange={setIsExpanded}
+        >
+          <DisclosureButton
+            className="flex w-full gap-6 border-gray-light"
+            withChevron
+            id={id}
+          >
+            Kjøpsalternativer
+          </DisclosureButton>
+          {!isExpanded && (
+            <ul className="flex gap-2" aria-label="Valgte kjøpsalternativer">
+              {selectedOptions.map((option) => (
+                <li key={option}>{option}</li>
+              ))}
+            </ul>
+          )}
+          {/* biome-ignore lint/a11y/useValidAriaRole: this is a custom component where role is a prop that defaults to 'group' */}
+          <DisclosurePanel className="px-4" role="none">
+            <CheckboxGroup
+              value={selectedOptions}
+              onChange={setSelectedItems}
+              aria-labelledby={id}
+            >
+              <Checkbox value="deleie">Deleie</Checkbox>
+              <Checkbox value="bostart">Bostart</Checkbox>
+              <Checkbox value="boligbytte">Boligbytte</Checkbox>
+            </CheckboxGroup>
+          </DisclosurePanel>
+        </Disclosure>
+      </div>
+    );
+  },
+};
+
+export const Grouped: Story = {
+  render: (props) => {
+    const id = useId();
+    const [selectedOptions, setSelectedItems] = useState<string[]>([]);
+    const [isExpanded, setIsExpanded] = useState(false);
+    return (
+      <DisclosureGroup className="grid gap-4 p-12">
+        <Disclosure
+          {...props}
+          isExpanded={isExpanded}
+          onExpandedChange={setIsExpanded}
+        >
+          <DisclosureButton
+            className="flex w-full gap-6 border-gray-light"
+            withChevron
+            id={id}
+          >
+            Kjøpsalternativer
+          </DisclosureButton>
+          {!isExpanded && (
+            <ul className="flex gap-2" aria-label="Valgte kjøpsalternativer">
+              {selectedOptions.map((option) => (
+                <li key={option}>{option}</li>
+              ))}
+            </ul>
+          )}
+          {/* biome-ignore lint/a11y/useValidAriaRole: this is a custom component where role is a prop that defaults to 'group' */}
+          <DisclosurePanel className="px-4" role="none">
+            <CheckboxGroup
+              value={selectedOptions}
+              onChange={setSelectedItems}
+              aria-labelledby={id}
+            >
+              <Checkbox value="deleie">Deleie</Checkbox>
+              <Checkbox value="bostart">Bostart</Checkbox>
+              <Checkbox value="boligbytte">Boligbytte</Checkbox>
+            </CheckboxGroup>
+          </DisclosurePanel>
+        </Disclosure>
+        <Disclosure
+          {...props}
+          isExpanded={isExpanded}
+          onExpandedChange={setIsExpanded}
+        >
+          <DisclosureButton
+            className="flex w-full gap-6 border-gray-light"
+            withChevron
+            id={id}
+          >
+            Område
+          </DisclosureButton>
+          {!isExpanded && (
+            <ul className="flex gap-2" aria-label="Valgte kjøpsalternativer">
+              {selectedOptions.map((option) => (
+                <li key={option}>{option}</li>
+              ))}
+            </ul>
+          )}
+          {/* biome-ignore lint/a11y/useValidAriaRole: this is a custom component where role is a prop that defaults to 'group' */}
+          <DisclosurePanel className="px-4" role="none">
+            <CheckboxGroup
+              value={selectedOptions}
+              onChange={setSelectedItems}
+              aria-labelledby={id}
+            >
+              <Checkbox value="oslo">Oslo</Checkbox>
+              <Checkbox value="bergen">Bergen</Checkbox>
+              <Checkbox value="trondheim">Trondheim</Checkbox>
+            </CheckboxGroup>
+          </DisclosurePanel>
+        </Disclosure>
+      </DisclosureGroup>
+    );
+  },
+};
---
+++ packages/react/src/disclosure/disclosure.tsx
@@ -0,0 +1,221 @@
+import { ChevronDown } from '@obosbbl/grunnmuren-icons-react';
+import { filterDOMProps } from '@react-aria/utils';
+import { type VariantProps, cva, cx } from 'cva';
+import {
+  type ForwardedRef,
+  type HTMLAttributes,
+  type HTMLProps,
+  type RefAttributes,
+  createContext,
+  useContext,
+  useId,
+  useRef,
+} from 'react';
+import { useDisclosure } from 'react-aria';
+import {
+  Button,
+  ButtonContext,
+  type ButtonProps,
+  type ContextValue,
+  DisclosureContext,
+  DisclosureGroup,
+  type DisclosureGroupProps,
+  DisclosureGroupStateContext,
+  Provider,
+  type DisclosureProps as RACDisclosureProps,
+  useContextProps,
+} from 'react-aria-components';
+import { useDisclosureState } from 'react-stately';
+
+const disclosureButtonVariants = cva({
+  base: [
+    'inline-flex cursor-pointer items-center justify-between rounded-lg outline-none data-[focus-visible]:outline-focus',
+    // Ensure a minimum click area of 44x44px, while making it look like it only has the size of the content
+    '-m-2.5 p-2.5 data-[focus-visible]:outline-offset-[-0.625rem]',
+  ],
+  variants: {
+    withChevron: {
+      true: '[&[aria-expanded="true"]_svg]:rotate-180',
+      false: null,
+    },
+    /**
+     * When the button is without text, but with a single icon.
+     * @default false
+     */
+    isIconOnly: {
+      true: '[&>svg]:h-7 [&>svg]:w-7',
+      false: 'gap-2.5',
+    },
+  },
+  defaultVariants: {
+    withChevron: false,
+    isIconOnly: false,
+  },
+});
+
+type DisclosureButtonProps = Omit<
+  ButtonProps,
+  'children' | 'aria-expanded' | 'aria-controls'
+> &
+  VariantProps<typeof disclosureButtonVariants> & {
+    children: React.ReactNode;
+  } & RefAttributes<HTMLButtonElement>;
+
+const DisclosureButton = ({
+  className,
+  withChevron,
+  isIconOnly,
+  children,
+  ref: _ref,
+  ...restProps
+}: DisclosureButtonProps) => {
+  const [props, ref] = useContextProps(
+    restProps,
+    _ref as ForwardedRef<HTMLButtonElement>,
+    ButtonContext,
+  );
+  return (
+    <Button
+      {...props}
+      ref={ref}
+      className={disclosureButtonVariants({
+        className,
+        withChevron,
+        isIconOnly,
+      })}
+    >
+      {children}
+      {withChevron && (
+        <ChevronDown className="flex-none transition-transform duration-300 motion-reduce:transition-none" />
+      )}
+    </Button>
+  );
+};
+
+type DisclosureProps = RACDisclosureProps &
+  RefAttributes<HTMLDivElement> & {
+    children: React.ReactNode;
+  };
+
+const Disclosure = ({ ref: _ref, children, ..._props }: DisclosureProps) => {
+  const [props, ref] = useContextProps(
+    _props,
+    _ref as ForwardedRef<HTMLDivElement>,
+    DisclosureContext,
+  );
+  const groupState = useContext(DisclosureGroupStateContext);
+
+  let { id, ...otherProps } = props;
+  const defaultId = useId();
+  id ||= defaultId;
+  const isExpanded = groupState
+    ? groupState.expandedKeys.has(id)
+    : props.isExpanded;
+
+  const state = useDisclosureState({
+    ...props,
+    isExpanded,
+    onExpandedChange(isExpanded) {
+      if (groupState) {
+        groupState.toggleKey(id);
+      }
+
+      props.onExpandedChange?.(isExpanded);
+    },
+  });
+
+  const isDisabled = props.isDisabled || groupState?.isDisabled || false;
+
+  const domProps = filterDOMProps(otherProps as HTMLProps<HTMLDivElement>);
+
+  const panelRef = useRef<HTMLDivElement | null>(null);
+  const { buttonProps, panelProps } = useDisclosure(
+    {
+      ...props,
+      isExpanded,
+      isDisabled,
+    },
+    state,
+    panelRef,
+  );
+
+  const { role: _, ...propsWithoutRole } = panelProps;
+
+  return (
+    <Provider
+      values={[
+        [DisclosureContext, state],
+        [ButtonContext, buttonProps],
+        [DisclosurePanelContext, { ...propsWithoutRole, ref: panelRef }],
+      ]}
+    >
+      <div
+        {...domProps}
+        ref={ref}
+        data-expanded={state.isExpanded || undefined}
+        data-disabled={isDisabled || undefined}
+      >
+        {children}
+      </div>
+    </Provider>
+  );
+};
+
+type DisclosurePanelProps = Omit<HTMLAttributes<HTMLDivElement>, 'role'> & {
+  children: React.ReactNode;
+  role?: 'group' | 'region' | 'none';
+} & RefAttributes<HTMLDivElement>;
+
+const DisclosurePanelContext = createContext<
+  ContextValue<Partial<DisclosurePanelProps>, HTMLDivElement>
+>({});
+
+const DisclosurePanel = ({ ref: _ref, ..._props }: DisclosurePanelProps) => {
+  const disclosureContext = useContext(
+    DisclosureContext,
+  ) as DisclosureProps | null;
+
+  const [props, ref] = useContextProps(
+    _props,
+    _ref as ForwardedRef<HTMLDivElement>,
+    DisclosurePanelContext,
+  );
+  const { role: _role = 'group', className, ...restProps } = props;
+  const ariaLabelledby =
+    _props['aria-labelledby'] ?? restProps['aria-labelledby'];
+  const isWithoutRole = _role === 'none';
+  const role = isWithoutRole ? undefined : _role;
+
+  return (
+    <div
+      className={cx(
+        'grid transition-all duration-300 after:relative after:block after:h-0 after:transition-all after:duration-300 motion-reduce:transition-none',
+        disclosureContext?.isExpanded
+          ? 'grid-rows-[1fr] after:h-3.5'
+          : 'grid-rows-[0fr]',
+      )}
+    >
+      <div
+        {...restProps}
+        ref={ref}
+        className={cx(
+          className,
+          'relative overflow-hidden [content-visibility:visible] before:relative before:block before:h-1.5 after:relative after:block after:h-1.5',
+        )}
+        role={role}
+        aria-labelledby={isWithoutRole ? undefined : ariaLabelledby}
+      />
+    </div>
+  );
+};
+
+export {
+  Disclosure as UNSAFE_Disclosure,
+  DisclosureButton as UNSAFE_DisclosureButton,
+  DisclosurePanel as UNSAFE_DisclosurePanel,
+  DisclosureGroup as UNSAFE_DisclosureGroup,
+  type DisclosureButtonProps as UNSAFE_DisclosureButtonProps,
+  type DisclosurePanelProps as UNSAFE_DisclosurePanelProps,
+  type DisclosureProps as UNSAFE_DisclosureProps,
+  type DisclosureGroupProps as UNSAFE_DisclosureGroupProps,
+};
