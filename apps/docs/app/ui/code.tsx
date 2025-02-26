87e8d7e5ec9a5958d606d72c3b2817be3024fcf6
Oscar Carlström <oscar.carlstrom@gmail.com>
2025-02-26 14:21:04 +0100

    Fix static code snippets

--- apps/docs/app/ui/code.tsx
+++ apps/docs/app/ui/code.tsx
@@ -12,14 +12,55 @@ const {
   Button,
 } = GrunnmurenScope;
 
-export type CodeProps = {
-  value: string;
-  setValue?: (newCode: string) => void;
+export type CodeProps = CodeSnippetProps & {
   caption?: string;
   /** @default false - Renders the code as a React component that is live editable */
   withLivePreview?: boolean;
   /** @default false - Makes the code editable */
   isEditable?: boolean;
+};
+
+type CodeSnippetProps = {
+  value: string;
+  setValue?: (newCode: string) => void;
+};
+
+const CodeSnippet = ({ value, setValue }: CodeSnippetProps) => {
+  const [hasCopied, setHasCopied] = useState(false);
+  // Use the same black color as the background on the editor (#1e1e1e)
+  return (
+    <div className="grid grid-cols-[1fr,auto] grid-rows-[auto,1fr] bg-[#1e1e1e]">
+      <LiveEditor
+        tabMode="focus"
+        className="row-span-2 font-mono"
+        onChange={setValue}
+      />
+      <div className="relative text-mint">
+        <button
+          className="size-[44px]"
+          onClick={() =>
+            navigator.clipboard.writeText(value).then(() => {
+              setHasCopied(true);
+              setTimeout(() => setHasCopied(false), 2000); // Reset after 2 seconds
+            })
+          }
+          type="button"
+        >
+          <GrunnmurenIconsScope.Documents />
+        </button>
+        <span
+          className={cx(
+            'absolute top-full right-2 transition-opacity duration-100',
+            hasCopied ? 'opacity-100' : 'opacity-0',
+          )}
+          aria-hidden={!hasCopied}
+          role="alert"
+        >
+          Kopiert!
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export const Code = ({
@@ -29,8 +70,6 @@ export const Code = ({
   withLivePreview,
   isEditable,
 }: CodeProps) => {
-  const [hasCopied, setHasCopied] = useState(false);
-
   const scope = withLivePreview
     ? // Scope is only needed when rendering the live preview
       { ...GrunnmurenIconsScope, ...GrunnmurenScope }
@@ -40,65 +79,38 @@ export const Code = ({
 
   return (
     <>
-      <div className="rounded-lg border">
+      <div className="overflow-hidden rounded-lg border">
         <LiveProvider
           code={value}
           scope={scope}
           theme={themes.vsDark}
           disabled={!isEditable}
         >
-          {withLivePreview && (
-            <div className="grid place-items-center p-18">
-              <LivePreview className="not-prose" />
-            </div>
-          )}
-          <Disclosure
-            isExpanded={isCodeExpanded}
-            onExpandedChange={setIsCodeExpanded}
-            className="grid grid-cols-1"
-          >
-            <Button
-              className="justify-self-end"
-              variant="tertiary"
-              slot="trigger"
-            >
-              {isCodeExpanded ? 'Skjul' : 'Vis'} kode
-            </Button>
-            <DisclosurePanel>
-              {/* Use the same black color as the background on the editor (#1e1e1e)  */}
-              <div className="grid grid-cols-[1fr,auto] grid-rows-[auto,1fr] overflow-hidden bg-[#1e1e1e]">
-                <LiveEditor
-                  tabMode="focus"
-                  className="row-span-2 font-mono"
-                  onChange={setValue}
-                />
-                <div className="relative text-mint">
-                  <button
-                    className="size-[44px]"
-                    onClick={() =>
-                      navigator.clipboard.writeText(value).then(() => {
-                        setHasCopied(true);
-                        setTimeout(() => setHasCopied(false), 2000); // Reset after 2 seconds
-                      })
-                    }
-                    type="button"
-                  >
-                    <GrunnmurenIconsScope.Documents />
-                  </button>
-                  <span
-                    className={cx(
-                      'absolute top-full right-2 transition-opacity duration-100',
-                      hasCopied ? 'opacity-100' : 'opacity-0',
-                    )}
-                    aria-hidden={!hasCopied}
-                    role="alert"
-                  >
-                    Kopiert!
-                  </span>
-                </div>
+          {withLivePreview ? (
+            <>
+              <div className="grid place-items-center p-18">
+                <LivePreview className="not-prose" />
               </div>
-            </DisclosurePanel>
-          </Disclosure>
+              <Disclosure
+                isExpanded={isCodeExpanded}
+                onExpandedChange={setIsCodeExpanded}
+                className="grid grid-cols-1"
+              >
+                <Button
+                  className="justify-self-end"
+                  variant="tertiary"
+                  slot="trigger"
+                >
+                  {isCodeExpanded ? 'Skjul' : 'Vis'} kode
+                </Button>
+                <DisclosurePanel>
+                  <CodeSnippet value={value} setValue={setValue} />
+                </DisclosurePanel>
+              </Disclosure>
+            </>
+          ) : (
+            <CodeSnippet value={value} />
+          )}
         </LiveProvider>
       </div>
       <p>{caption}</p>
