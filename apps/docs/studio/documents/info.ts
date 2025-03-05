25310ff4c3bf7c03bff63461b4e2b626ebc168c7
Oscar Carlström <oscar.carlstrom@gmail.com>
2025-03-05 10:00:00 +0100

    Add info document schema

---
+++ apps/docs/studio/documents/info.ts
@@ -0,0 +1,63 @@
+import { defineField, defineType } from 'sanity';
+
+export default defineType({
+  name: 'info',
+  title: 'Info',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+      },
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'content',
+      title: 'Content',
+      type: 'content',
+    }),
+    defineField({
+      name: 'resourceLinks',
+      title: 'Resource links',
+      description:
+        'Add links to GitHub or Figma here (only GitHub and Figma is supported)',
+      type: 'array',
+      of: [
+        {
+          type: 'object',
+          name: 'resourceLink',
+          title: 'Resource link',
+          fields: [
+            {
+              name: 'linkType',
+              type: 'string',
+              title: 'Link type',
+              options: {
+                list: [
+                  { title: 'Figma', value: 'figma' },
+                  { title: 'GitHub', value: 'github' },
+                ],
+              },
+              validation: (Rule) => Rule.required(),
+            },
+            {
+              name: 'url',
+              type: 'url',
+              title: 'URL',
+              validation: (Rule) => Rule.required(),
+            },
+          ],
+        },
+      ],
+    }),
+  ],
+});
