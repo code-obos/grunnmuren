adf9e6518b634b394d8a807fa1dea0e0a2b26d2f
Oscar Carlström <oscar.carlstrom@gmail.com>
2025-03-13 12:49:51 +0100

    Prevent form submission on invalid field

--- packages/react/src/file-upload/file-upload.tsx
+++ packages/react/src/file-upload/file-upload.tsx
@@ -4,6 +4,7 @@ import {
   type Dispatch,
   type ReactNode,
   type SetStateAction,
+  useCallback,
   useEffect,
   useId,
   useRef,
@@ -11,8 +12,6 @@ import {
 } from 'react';
 import {
   ButtonContext,
-  FieldError,
-  FieldErrorContext,
   FormContext,
   InputContext,
   Provider,
@@ -21,12 +20,12 @@ import {
 import { FileTrigger, type FileTriggerProps } from './file-trigger';
 import { type Locale, useLocale } from '../use-locale';
 import { ErrorMessage } from '../label';
-import { formFieldError } from '../classes';
 import { useFormValidation } from '@react-aria/form';
 import { useFormValidationState } from '@react-stately/form';
 import { useControlledState } from '@react-stately/utils';
 import { useField } from 'react-aria';
-import { useFormReset, useUpdateEffect } from '@react-aria/utils';
+import { useFormReset } from '@react-aria/utils';
+import { ErrorMessageOrFieldError } from '../label/error-messsage-or-field-error';
 
 type Props = Omit<FileTriggerProps, 'onSelect'> & {
   label: string;
@@ -129,66 +128,63 @@ const FileUpload = ({
   const validationBehavior =
     fileTriggerProps.validationBehavior ??
     slottedContext.validationBehavior ??
-    'aria';
+    'native';
 
-  const validateAll = (files: File[]) => {
-    if (validate === undefined) return true;
+  const validateFiles = useCallback(
+    (files: File[]) => {
+      if (validate === undefined) return true;
 
-    const errors = [] as string[];
-    for (const file of files) {
-      if (!validate) continue;
-      const validation = validate(file);
-      if (typeof validation === 'string') errors.push(validation);
-    }
-    if (errors.length === 0) return true;
+      const errors = [] as string[];
+      for (const file of files) {
+        if (!validate) continue;
+        const validation = validate(file);
+        if (typeof validation === 'string') errors.push(validation);
+      }
+      if (errors.length === 0) return true;
 
-    return errors;
-  };
+      return errors;
+    },
+    [validate],
+  );
 
   const validationState = useFormValidationState({
     ...fileTriggerProps,
     validationBehavior,
-    validate: validateAll,
+    validate: validateFiles,
     isRequired,
     isInvalid,
     value: _files ?? files ?? null,
   });
 
+  const controlledOrUncontrolledFiles =
+    // Use controlled files if they are provided, otherwise use internal files - but map them to File objects (remove validation prop)
+    _files ?? files;
   useEffect(() => {
     // Keep the native file input in sync with the internal file state
     if (inputRef.current) {
-      const controlledOrUncontrolledFiles =
-        // Use controlled files if they are provided, otherwise use internal files - but map them to File objects (remove validation prop)
-        _files ?? files.map((file) => new File([file], file.name));
       inputRef.current.files = filesToDataTransfer(
         controlledOrUncontrolledFiles,
       );
     }
-  }, [files, _files, inputRef]);
+  }, [controlledOrUncontrolledFiles, inputRef]);
 
-  useUpdateEffect(() => {
-    validationState.commitValidation();
-  }, [files, _files]);
-
-  const { errorMessageProps, fieldProps } = useField({
+  const { fieldProps } = useField({
     ...fileTriggerProps,
-    validate: validateAll,
+    validate: validateFiles,
     validationBehavior,
     isInvalid,
-    errorMessage:
-      errorMessage ?? validationState.displayValidation.validationErrors,
+    errorMessage,
   });
 
   const [value, setValue] = useControlledState<File[]>(_files, [], onChange);
   useFormReset(inputRef, value, setValue);
 
-  // TODO: Prevent the form from submitting if the file input is invalid
   // TODO: Dislpay error message when file input is required and empty
   useFormValidation(
     {
       ...fileTriggerProps,
       validationBehavior,
-      validate: validateAll,
+      validate: validateFiles,
       isRequired,
       isInvalid,
     },
@@ -197,115 +193,113 @@ const FileUpload = ({
   );
 
   return (
-    <Provider values={[[FieldErrorContext, validationState.displayValidation]]}>
-      <div
-        data-slot="file-upload"
-        className="group grid w-72 max-w-full gap-2"
-        data-required={isRequired}
+    <div
+      data-slot="file-upload"
+      className="group grid w-72 max-w-full gap-2"
+      data-required={isRequired}
+    >
+      <label htmlFor={id} className="font-semibold">
+        {label}
+      </label>
+      <Provider
+        values={[
+          [
+            ButtonContext,
+            {
+              // The button acts as the trigger for the file input, which is why we connect the label to the button id
+              id,
+            },
+          ],
+          [InputContext, fieldProps],
+        ]}
       >
-        <label htmlFor={id} className="font-semibold">
-          {label}
-        </label>
-        <Provider
-          values={[
-            [
-              ButtonContext,
-              {
-                // The button acts as the trigger for the file input, which is why we connect the label to the button id
-                id,
-              },
-            ],
-            [InputContext, fieldProps],
-          ]}
-        >
-          <FileTrigger
-            {...fileTriggerProps}
-            isRequired={isRequired}
-            allowsMultiple={allowsMultiple}
-            onSelect={(selectedFiles) => {
-              if (selectedFiles === null) return;
+        <FileTrigger
+          {...fileTriggerProps}
+          isRequired={isRequired}
+          allowsMultiple={allowsMultiple}
+          onSelect={(selectedFiles) => {
+            if (selectedFiles === null) return;
 
-              const newFiles = Array.from(selectedFiles);
+            const newFiles = Array.from(selectedFiles);
 
-              // For controlled component
-              onChange?.((prevFiles) =>
-                allowsMultiple
-                  ? uniqueFileNames(prevFiles.concat(newFiles))
-                  : newFiles,
-              );
-
-              // For internal file state
-              setFiles((prevFiles) =>
-                allowsMultiple
-                  ? uniqueFileNames(prevFiles.concat(newFiles))
-                  : newFiles,
-              );
-            }}
-            isInvalid={isInvalid}
-            ref={inputRef}
-          >
-            {children}
-          </FileTrigger>
-        </Provider>
-        <ul className="mt-4 grid gap-y-2">
-          {files.map((file, fileIndex) => {
-            let fileName = file.name;
-            if (
-              fileTriggerProps.acceptDirectory &&
-              file.webkitRelativePath !== ''
-            ) {
-              fileName = file.webkitRelativePath;
-            }
-
-            const validation = validate?.(file) ?? true;
-            const hasError = validation !== true;
-
-            return (
-              <li key={fileName}>
-                <div
-                  className={cx(
-                    'flex items-center justify-between gap-2 rounded-lg border-2 px-4 py-2',
-                    hasError
-                      ? 'border-red bg-red-light'
-                      : 'border-gray bg-gray-lightest',
-                  )}
-                >
-                  {fileName}{' '}
-                  <button
-                    className={cx(
-                      '-m-2 grid h-11 w-11 shrink-0 place-items-center rounded-xl',
-                      // Focus styles
-                      'focus-visible:-outline-offset-8 focus-visible:outline-focus',
-                    )}
-                    onClick={() => {
-                      // For controlled component
-                      onChange?.((prevFiles) =>
-                        prevFiles.filter((_, index) => index !== fileIndex),
-                      );
-
-                      // For internal file state
-                      setFiles((prevFiles) =>
-                        prevFiles.filter((_, index) => index !== fileIndex),
-                      );
-                    }}
-                    aria-label={translations.remove[locale]}
-                    type="button"
-                  >
-                    <Trash />
-                  </button>
-                </div>
-                {hasError && (
-                  <ErrorMessage className="mt-1 block w-full">
-                    {validation}
-                  </ErrorMessage>
-                )}
-              </li>
+            // For controlled component
+            onChange?.((prevFiles) =>
+              allowsMultiple
+                ? uniqueFileNames(prevFiles.concat(newFiles))
+                : newFiles,
             );
-          })}
-        </ul>
-        <FieldError {...errorMessageProps} className={formFieldError} />
-      </div>
-    </Provider>
+
+            // For internal file state
+            setFiles((prevFiles) =>
+              allowsMultiple
+                ? uniqueFileNames(prevFiles.concat(newFiles))
+                : newFiles,
+            );
+          }}
+          isInvalid={isInvalid || validationState.displayValidation.isInvalid}
+          ref={inputRef}
+        >
+          {children}
+        </FileTrigger>
+      </Provider>
+      <ul className="mt-4 grid gap-y-2">
+        {controlledOrUncontrolledFiles.map((file, fileIndex) => {
+          let fileName = file.name;
+          if (
+            fileTriggerProps.acceptDirectory &&
+            file.webkitRelativePath !== ''
+          ) {
+            fileName = file.webkitRelativePath;
+          }
+
+          const validation = validate?.(file) ?? true;
+          const hasError = validation !== true;
+
+          return (
+            <li key={fileName}>
+              <div
+                className={cx(
+                  'flex items-center justify-between gap-2 rounded-lg border-2 px-4 py-2',
+                  hasError
+                    ? 'border-red bg-red-light'
+                    : 'border-gray bg-gray-lightest',
+                )}
+              >
+                {fileName}{' '}
+                <button
+                  className={cx(
+                    '-m-2 grid h-11 w-11 shrink-0 place-items-center rounded-xl',
+                    // Focus styles
+                    'focus-visible:-outline-offset-8 focus-visible:outline-focus',
+                  )}
+                  onClick={() => {
+                    // For controlled component
+                    onChange?.((prevFiles) =>
+                      prevFiles.filter((_, index) => index !== fileIndex),
+                    );
+
+                    // For internal file state
+                    setFiles((prevFiles) =>
+                      prevFiles.filter((_, index) => index !== fileIndex),
+                    );
+                  }}
+                  aria-label={translations.remove[locale]}
+                  type="button"
+                >
+                  <Trash />
+                </button>
+              </div>
+              {hasError && (
+                <ErrorMessage className="mt-1 block w-full">
+                  {validation}
+                </ErrorMessage>
+              )}
+            </li>
+          );
+        })}
+      </ul>
+      <ErrorMessageOrFieldError errorMessage={errorMessage} />
+    </div>
   );
 };
 
