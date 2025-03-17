95307cb5497a0f7f24a5169f69ffa06c32be70ef
Gabriel Wallin <gabriel.wallin@bekk.no>
2025-03-17 13:03:53 +0100

    Fix ref type

--- packages/react/src/tag-group/tag-group.tsx
+++ packages/react/src/tag-group/tag-group.tsx
@@ -1,5 +1,6 @@
-import { Close } from "@obosbbl/grunnmuren-icons-react";
-import { cva } from "cva";
+import { Close } from '@obosbbl/grunnmuren-icons-react';
+import { cva } from 'cva';
+import type { RefAttributes } from 'react';
 import {
   Button,
   Tag as RACTag,
@@ -8,54 +9,57 @@ import {
   TagList as RACTagList,
   type TagListProps as RACTagListProps,
   type TagProps as RACTagProps,
-} from "react-aria-components";
+} from 'react-aria-components';
 
 const tagVariants = cva({
   base: [
-    "inline-flex cursor-pointer items-center gap-2 rounded-lg font-medium text-sm transition-colors duration-200",
+    'inline-flex cursor-pointer items-center gap-2 rounded-lg font-medium text-sm transition-colors duration-200',
     //Focus
-    "focus-visible:outline-focus-offset [&:not([data-focus-visible])]:outline-none",
+    'focus-visible:outline-focus-offset [&:not([data-focus-visible])]:outline-none',
     //Border
-    "shadow-[inset_0_0_0_2px_#002169]",
+    'shadow-[inset_0_0_0_2px_#002169]',
     //Backgrounds
     "hover:!bg-sky bg-white text-black aria-selected:bg-sky-light [&:has([slot='remove'])]:bg-sky-light",
   ],
 });
 
 const removeButtonStyling =
-  "focus-visible:outline-focus-offset [&:not([data-focus-visible])]:outline-none flex w-full items-center px-3 py-1.5";
+  'focus-visible:outline-focus-offset [&:not([data-focus-visible])]:outline-none flex w-full items-center px-3 py-1.5';
 
-export type TagGroupProps = Omit<RACTagGroupProps, "className"> & {
-  /**
-   * The selection mode of the tag group
-   * @default single
-   */
-  selectionMode?: "single" | "multiple";
-  /**
-   * CSS classes to apply to the tag group
-   */
-  className?: string;
-  /**
-   * The function to call when the tag is removed
-   */
-  onRemove?: (key: React.Key) => void;
-};
+export type TagGroupProps = Omit<RACTagGroupProps, 'className'> &
+  RefAttributes<HTMLDivElement> & {
+    /**
+     * The selection mode of the tag group
+     * @default single
+     */
+    selectionMode?: 'single' | 'multiple';
+    /**
+     * CSS classes to apply to the tag group
+     */
+    className?: string;
+    /**
+     * The function to call when the tag is removed
+     */
+    onRemove?: (key: React.Key) => void;
+  };
 
 //The usage of <object> here could probably be replaced with a generic for more type safety in usage
-export type TagListProps = Omit<RACTagListProps<object>, "className"> & {
-  /**
-   * CSS classes to apply to the tag list
-   */
-  className?: string;
-};
+export type TagListProps = Omit<RACTagListProps<object>, 'className'> &
+  RefAttributes<HTMLDivElement> & {
+    /**
+     * CSS classes to apply to the tag list
+     */
+    className?: string;
+  };
 
-export type TagProps = Omit<RACTagProps, "className"> & {
-  children: React.ReactNode;
-  /**
-   * CSS classes to apply to the tag
-   */
-  className?: string;
-};
+export type TagProps = Omit<RACTagProps, 'className'> &
+  RefAttributes<HTMLDivElement> & {
+    children: React.ReactNode;
+    /**
+     * CSS classes to apply to the tag
+     */
+    className?: string;
+  };
 
 /**
  * A group component for Tag components that enables selection and organization of options.
@@ -79,7 +83,7 @@ function TagGroup(props: TagGroupProps) {
  * A container component for Tag components within a TagGroup.
  */
 function TagList(props: TagListProps) {
-  const { className = "flex flex-wrap gap-2", children, ...restProps } = props;
+  const { className = 'flex flex-wrap gap-2', children, ...restProps } = props;
 
   return (
     <RACTagList {...restProps} className={className}>
@@ -94,7 +98,7 @@ function TagList(props: TagListProps) {
 function Tag(props: TagProps) {
   const { className, children, ...restProps } = props;
 
-  const spanStyling = "flex items-center gap-1 align-middle";
+  const spanStyling = 'flex items-center gap-1 align-middle';
 
   return (
     <RACTag
@@ -105,7 +109,7 @@ function Tag(props: TagProps) {
     >
       {({ allowsRemoving }) =>
         allowsRemoving ? (
-          <Button className={removeButtonStyling} slot={"remove"}>
+          <Button className={removeButtonStyling} slot={'remove'}>
             <span className={spanStyling}>{children}</span>
             <Close className="ml-1 h-4 w-4" />
           </Button>
