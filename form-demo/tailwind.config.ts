1c920c67cb24ecab07faab4744708dacf81b6f11
Oscar Carlström <oscar.carlstrom@gmail.com>
2025-04-04 14:50:47 +0200

    Remove legacy config

--- form-demo/tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  presets: [require('@obosbbl/grunnmuren-tailwind')],
-  content: [
-    './app/**/*.{tsx,ts}',
-    './node_modules/@obosbbl/grunnmuren-react/dist/**/*.{mjs,js}',
-  ],
-};
