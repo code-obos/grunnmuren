aff2283b16004e1d7d16da0e5b8ef1559e9324fa
Oscar Carlström <oscar.carlstrom@gmail.com>
2025-05-13 09:11:54 +0200

    Extract colors from tw css config

---
+++ apps/docs/extract-colors.js
@@ -0,0 +1,33 @@
+import fs from 'node:fs';
+
+const outputPath = './colors.ts';
+console.log(`Writing to "${outputPath}"...\n`);
+
+try {
+  const file = fs.readFileSync(
+    'node_modules/@obosbbl/grunnmuren-tailwind/tailwind-base.css',
+  );
+  const colors = Object.fromEntries(
+    file
+      .toString()
+      .split('\n')
+      .filter((line) => line.includes('color-'))
+      .map((line) => {
+        const [name, value] = line.split(':');
+        return [
+          name.replace('--color-', '').trim(),
+          value.replace(';', '').trim(),
+        ];
+      }),
+  );
+
+  fs.writeFileSync(
+    outputPath,
+    `export default ${JSON.stringify(colors, null, 2)}\n`,
+  );
+  console.log('\x1b[32m%s\x1b[0m', 'Successfully wrote colors to file!');
+} catch (error) {
+  console.error('\x1b[31m%s\x1b[0m', `\nSomething went wrong: ${error}`);
+}
+
+console.log(`Done writing to "${outputPath}"...\n`);
