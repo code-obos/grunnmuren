c6f63048a6baec76875aea4bc6221154b78bfbf1
Oscar Carlström <oscar.carlstrom@gmail.com>
2025-06-05 12:18:50 +0200

    Update examples

--- packages/react/src/hero/hero.stories.tsx
+++ packages/react/src/hero/hero.stories.tsx
@@ -203,9 +203,10 @@ export const FullBleedWithCarousel = () => (
       <Media>
         <Carousel>
           <CarouselItem>
-            <img
-              src="https://res.cloudinary.com/obosit-prd-ch-clry/image/upload/f_auto,c_limit,w_2048,q_auto/v1582122753/Boligprosjekter/Oslo/Ulven/Ulven-N%C3%A6romr%C3%A5de-Oslo-OBOS-Construction-city.jpg"
-              alt=""
+            <VideoLoop
+              src="https://res.cloudinary.com/obosit-prd-ch-clry/video/upload/v1732199756/Mellom%20husene/Frysja_Loop2.mp4"
+              format="mp4"
+              alt="En postbil kjører rundt i det moderne nabolaget på Frysja. Her finnes det fine uteområder, med husker og kafeer."
             />
           </CarouselItem>
           <CarouselItem>
