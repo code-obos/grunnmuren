b15fd58019f079c87477a39f429a2b2b606195a3
Aulon Mujaj <4094284+aulonm@users.noreply.github.com>
2025-10-27 18:54:04 +0100

    fix stories prop in storybook config to not traverse node_modules

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,7 @@ import type { StorybookConfig } from '@storybook/react-vite';
 import { mergeConfig } from 'vite';
 
 const config: StorybookConfig = {
-  stories: ['../packages/react/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  stories: ['../packages/react/src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: ['@storybook/addon-docs'],
 
   framework: {
