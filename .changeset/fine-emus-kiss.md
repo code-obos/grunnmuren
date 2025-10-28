fdaaed06afde96c875d9fe884c3f2d2d7bd0160e
Oscar Carlström <oscar.carlstrom@gmail.com>
2025-10-28 11:58:14 +0100

    Pass all HTML props to the Hero component

---
+++ .changeset/fine-emus-kiss.md
@@ -0,0 +1,5 @@
+---
+"@obosbbl/grunnmuren-react": patch
+---
+
+Pass on all HTML props to the `Hero` component.
--- packages/react/src/hero/hero.tsx
+++ packages/react/src/hero/hero.tsx
@@ -92,7 +92,7 @@ const variants = cva({
   },
 });
 
-const Hero = ({ variant, className, children }: HeroProps) => {
+const Hero = ({ variant, className, ...rest }: HeroProps) => {
   const variantsClassName = variants({
     variant,
     className,
@@ -121,7 +121,7 @@ const Hero = ({ variant, className, children }: HeroProps) => {
         ],
       ]}
     >
-      <div className={cx(variantsClassName, className)}>{children}</div>
+      <div className={cx(variantsClassName, className)} {...rest} />
     </Provider>
   );
 };
