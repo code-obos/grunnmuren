ae42085401451d22ed5b81cf9d27e90b04c4c85d
Aulon Mujaj <4094284+aulonm@users.noreply.github.com>
2025-11-12 12:09:34 +0100

    add consent and amplitude scripts

--- apps/docs/src/routes/__root.tsx
+++ apps/docs/src/routes/__root.tsx
@@ -26,6 +26,30 @@ function RootDocument() {
     <html lang="no">
       <head>
         <HeadContent />
+        {process.env.NODE_ENV === 'development' && (
+          <>
+            <script
+              src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js"
+              data-language="no"
+              type="text/javascript"
+              charset="UTF-8"
+              data-domain-script="ea448f9e-5a74-4f6e-9260-c61b81f67013"
+            ></script>
+            <script type="text/javascript">function OptanonWrapper() {}</script>
+            <script
+              src="https://www.obos.no/collector.js"
+              strategy="afterInteractive"
+              type="module"
+              id="collector-script"
+              data-api-key="4426822b50f2893bb0bc6cd74521848c"
+              data-app-name="grunnmuren"
+              data-collect-url="/collect"
+              data-session-replay-collect-url="/collect-sr"
+              data-enable-session-replay="true"
+              crossOrigin=""
+            />
+          </>
+        )}
       </head>
       {/* Setting the body position to relative enables setting the backdrop on expanded menu */}
       <body className="relative">
