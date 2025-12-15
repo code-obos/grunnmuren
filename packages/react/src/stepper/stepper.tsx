58cc235a19ef0038d5f198f9cb09c228f3200988
Oscar Carlström <oscar.carlstrom@gmail.com>
2025-12-15 16:15:44 +0100

    Remove redundant comments

--- packages/react/src/stepper/stepper.tsx
+++ packages/react/src/stepper/stepper.tsx
@@ -20,8 +20,6 @@ import { ScrollButton, useHorizontalScroll } from '../utils';
 type StepProps = HTMLProps<HTMLLIElement> & {
   /**
    * Indicates whether the step is completed or not.
-   * A completed step means that the user has filled out and submitted the corresponding section.
-   * And that all the data entered in that section is valid.
    * @default false
    */
   isCompleted?: boolean;
