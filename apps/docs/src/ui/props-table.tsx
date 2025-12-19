194233a1d6656d5b65634773c4b137ca22d71dcd
Alexander Bjerkan <alexander.bjerkan@obos.no>
2025-12-19 09:45:34 +0100

    docs: group props in prop table

--- apps/docs/src/ui/props-table.tsx
+++ apps/docs/src/ui/props-table.tsx
@@ -1,4 +1,5 @@
 import * as props from 'component-props';
+import type { PropItem } from 'react-docgen-typescript';
 import { AnchorHeading } from './anchor-heading';
 import { Table, TableBody, TableCell, TableHead, TableRow } from './table';
 
@@ -8,6 +9,19 @@ interface PropsTableProps {
 
 export const PropsTable = ({ componentName }: PropsTableProps) => {
   const headingId = `${componentName.toLowerCase()}-props`;
+
+  const componentProps = Object.values(props[componentName].props);
+
+  const groupedProps = Object.groupBy(componentProps, (prop) => {
+    if (prop.name.startsWith('on')) {
+      return 'Events';
+    } else if (prop.name.startsWith('aria-')) {
+      return 'Accessibility';
+    } else {
+      return 'Props';
+    }
+  });
+
   return (
     <div className="overflow-x-auto">
       <AnchorHeading className="heading-s my-2" level={2} id={headingId}>
@@ -22,20 +36,48 @@ export const PropsTable = ({ componentName }: PropsTableProps) => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {Object.values(props[componentName].props).map((prop) => (
-            <TableRow key={prop.name}>
-              <TableCell className="italic">
-                <code className="font-mono">
-                  {prop.name}
-                  {prop.required ? '' : '?'}
-                </code>
-              </TableCell>
-              <TableCell>{prop.description}</TableCell>
-              <TableCell>{prop.defaultValue?.value ?? '-'}</TableCell>
-            </TableRow>
-          ))}
+          <PropRows props={groupedProps.Props} />
+          <PropRows props={groupedProps.Events} heading="Events" />
+          <PropRows
+            props={groupedProps.Accessibility}
+            heading="Accessibility"
+          />
         </TableBody>
       </Table>
     </div>
+  );
+};
+
+const PropRows = ({
+  props,
+  heading,
+}: {
+  props?: Array<PropItem>;
+  heading?: string;
+}) => {
+  if (!props) return null;
+
+  return (
+    <>
+      {heading && (
+        <TableRow>
+          <th colSpan={3} className="font-bold">
+            {heading}
+          </th>
+        </TableRow>
+      )}
+      {props.map((prop) => (
+        <TableRow key={prop.name}>
+          <TableCell className="italic">
+            <code className="font-mono">
+              {prop.name}
+              {prop.required ? '' : '?'}
+            </code>
+          </TableCell>
+          <TableCell>{prop.description}</TableCell>
+          <TableCell>{prop.defaultValue?.value ?? '-'}</TableCell>
+        </TableRow>
+      ))}
+    </>
   );
 };
