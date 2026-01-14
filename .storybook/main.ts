8415d5e4a4800a986b7399dbc2a11aa27881699e
Alexander Bjerkan <alexander.bjerkan@obos.no>
2026-01-14 09:41:36 +0100

    storybook: remove tailwindcss/vite workaround

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,6 @@
 import optimizeLocales from '@react-aria/optimize-locales-plugin';
 import type { StorybookConfig } from '@storybook/react-vite';
+import tailwindcss from '@tailwindcss/vite';
 import { mergeConfig } from 'vite';
 
 const config: StorybookConfig = {
@@ -29,9 +30,7 @@ const config: StorybookConfig = {
           }),
           enforce: 'pre',
         },
-        // temp fix for Storybook using CJS
-        // see https://github.com/tailwindlabs/tailwindcss/discussions/16687#discussioncomment-12374574
-        (await import('@tailwindcss/vite')).default(),
+        tailwindcss(),
       ],
     });
   },
