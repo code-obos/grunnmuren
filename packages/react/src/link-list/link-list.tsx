f5df9afc94d0a1c65f398d78acfb5f82373e436e
Oscar Carlström <oscar.carlstrom@gmail.com>
2026-01-22 10:47:15 +0100

    Move tw classes

--- packages/react/src/link-list/link-list.tsx
+++ packages/react/src/link-list/link-list.tsx
@@ -3,7 +3,7 @@ import {
   Download,
   LinkExternal,
 } from '@obosbbl/grunnmuren-icons-react';
-import { cva, type VariantProps } from 'cva';
+import { cva, cx, type VariantProps } from 'cva';
 import {
   Children,
   cloneElement,
@@ -15,8 +15,26 @@ import { Provider } from 'react-aria-components';
 import { HeadingContext } from '../content';
 import type { UNSAFE_LinkProps } from '../link/link';
 
+const linkStyles = [
+  '*:data-[slot=link]:flex',
+  '*:data-[slot=link]:w-full',
+  '*:data-[slot=link]:justify-between',
+  '*:data-[slot=link]:gap-x-2',
+  '*:data-[slot=link]:py-3.5',
+  '*:data-[slot=link]:no-underline',
+  '*:data-[slot=link]:focus-visible:outline-focus',
+];
+
 const linkListContainerVariants = cva({
-  base: null,
+  base: [
+    '*:data-[slot=link-list]:overflow-visible',
+
+    '*:data-[slot=heading]:p-1.25',
+    '*:data-[slot=heading]:*:data-[slot=link]:py-2.25',
+    '*:data-[slot=heading]:*:data-[slot=link]:[svg]:text-base', // THIS WORK?
+    'has-data-[slot=heading]:*:data-[slot=link-list]:overflow-visible',
+    '*:data-[slot=heading]:has-not:*:data-[slot=link]:my-2.25',
+  ],
   variants: {
     layout: {
       stack: null,
@@ -38,11 +56,14 @@ const LinkListContainer = ({
   layout = 'stack',
   ...props
 }: LinkListContainerProps) => (
-  <Provider values={[[HeadingContext, { size: 'm' }]]}>
+  <Provider
+    values={[[HeadingContext, { size: 'm', className: cx(linkStyles) }]]}
+  >
     <div
       {...props}
       className={linkListContainerVariants({ className, layout })}
       data-layout={layout}
+      data-slot="link-list-container"
     />
   </Provider>
 );
@@ -51,22 +72,42 @@ type LinkListProps = React.HTMLProps<HTMLUListElement> & {
   children: JSX.Element | JSX.Element[];
 };
 
-const LinkList = (props: LinkListProps) => (
-  <ul {...props} data-slot="link-list" />
+const LinkList = ({ className, ...restProps }: LinkListProps) => (
+  <ul
+    {...restProps}
+    data-slot="link-list"
+    className={cx(
+      /**
+       * Hides dividers at the top of the list (overflow-y)
+       * while preventing arrow icons from overflowing container when animated to the right (overflow-x)
+       */
+      'grid min-w-fit auto-rows-max gap-y-px overflow-hidden',
+      className,
+    )}
+  />
 );
 
 type LinkListItemProps = React.HTMLProps<HTMLLIElement> & {
   children: ReactNode;
 };
 
-const LinkListItem = ({ children, ...props }: LinkListItemProps) => {
+const LinkListItem = ({ children, className, ...props }: LinkListItemProps) => {
   const child = Children.only(children);
   const childProps = (
     isValidElement(child) ? child.props : {}
   ) as UNSAFE_LinkProps;
 
   return (
-    <li {...props} data-slot="link-list-item">
+    <li
+      {...props}
+      className={cx(
+        className,
+        'after:-top-px relative p-1.25 after:absolute after:right-0 after:left-0 after:h-px after:w-full after:bg-gray-light',
+        '*:data-[slot=link]:paragraph',
+        ...linkStyles,
+      )}
+      data-slot="link-list-item"
+    >
       {isValidElement(child) &&
         cloneElement(child, {
           animateIcon: childProps.download
